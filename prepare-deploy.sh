5ab37cabd414a8712e83065adb5804e290439e17
PavloSukhinin <pavel.sukhinin@binary-studio.com>
2022-07-31 15:24:52 +0300

    GRHB-4: * mv build process out of actions

--- prepare-deploy.sh
+++ prepare-deploy.sh
@@ -4,5 +4,3 @@ mkdir -p ./build/backend/; mv ./backend/build/* ./build/backend/
 mkdir ./build/shared/; mv ./shared/build ./build/shared/
 mv ./frontend/build/* ./build/backend/public
 cp package.json package-lock.json ./build
-
-cd build && zip -r build.zip . && cd -
