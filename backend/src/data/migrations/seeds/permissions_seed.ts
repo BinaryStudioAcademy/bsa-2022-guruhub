ff398998a11e376a81255996bafa97aabd5be084
Oleksandr Lokhonia <36548565+olokhonia@users.noreply.github.com>
2022-08-06 22:16:27 +0300

    GRHB-44: * update backend/src/data/migrations/seeds/permissions_seed.ts
    Co-authored-by: Andrii Fedorchenko <69236539+andanfe@users.noreply.github.com>

--- backend/src/data/migrations/seeds/permissions_seed.ts
+++ backend/src/data/migrations/seeds/permissions_seed.ts
@@ -5,5 +5,5 @@ const TABLE_NAME = 'permissions';
 export async function seed(knex: Knex): Promise<void> {
   await knex(TABLE_NAME).del();
 
-  await knex(TABLE_NAME).insert([{ id: 1, name: 'Manage UAM', key: 'UAM' }]);
+  await knex(TABLE_NAME).insert([{ id: 1, name: 'Manage UAM', key: 'manage_uam' }]);
 }
