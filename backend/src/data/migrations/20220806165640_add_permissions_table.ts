2bdb0c5a96c41f19f43ed2a2592c9b8c0d07b503
Oleksandr Lokhonia <oleksandr_lokhonia@epam.com>
2022-08-06 20:11:25 +0300

    + permissions migration

---
+++ backend/src/data/migrations/20220806165640_add_permissions_table.ts
@@ -0,0 +1,30 @@
+import { Knex } from 'knex';
+
+enum ColumnName {
+  ID = 'id',
+  NAME = 'name',
+  KEY = 'key',
+  CREATED_AT = 'created_at',
+  UPDATED_AT = 'updated_at',
+}
+const TABLE_NAME = 'permissions';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments(ColumnName.ID).primary();
+    table.string(ColumnName.NAME).unique().notNullable();
+    table.string(ColumnName.KEY).unique().notNullable();
+    table
+      .dateTime(ColumnName.CREATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .dateTime(ColumnName.UPDATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+}
