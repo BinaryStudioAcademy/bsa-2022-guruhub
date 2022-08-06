d29b3933b5fb09d64a5c1e44e6778ed18cbc3a7e
Kirill Katkov <katkov.kirill10@gmail.com>
2022-08-06 22:43:44 +0300

    GRHB-37: * another fix about folder name error

--- backend/src/exceptions/exceptions.ts
+++ backend/src/exceptions/exceptions.ts
@@ -1,2 +1,2 @@
 export { HttpError } from 'guruhub-shared/exceptions/exceptions';
-export { BadCredentials } from './BadCredentials/bad-credentials.exception';
+export { BadCredentials } from './bad-credentials/bad-credentials.exception';
