5c043474d12930205a2eb6ac5af8bd44b744def1
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-08-06 21:21:40 +0300

    GRHB-11: * table component removed from app component

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -4,36 +4,9 @@ import { Routes, Route, Link } from 'components/common/common';
 import { useLocation } from 'hooks/hooks';
 import { Auth } from 'components/auth/auth';
 import logo from 'assets/img/logo.svg';
-import { Table } from 'components/common/table/table';
 
 const App: FC = () => {
   const { pathname } = useLocation();
-
-  const testRowsData = [
-    {
-      col1: 'Hello',
-      col2: 'World',
-    },
-    {
-      col1: 'react-table',
-      col2: 'rocks',
-    },
-    {
-      col1: 'whatever',
-      col2: 'you want',
-    },
-  ];
-
-  const testColumnsData = [
-    {
-      Header: 'Column 1',
-      accessor: 'col1', // accessor is the "key" in the data
-    },
-    {
-      Header: 'Column 2',
-      accessor: 'col2',
-    },
-  ];
 
   return (
     <>
@@ -59,7 +32,6 @@ const App: FC = () => {
           <Route path={AppRoute.SIGN_IN} element={<Auth />} />
         </Routes>
       </div>
-      <Table columns={testColumnsData} data={testRowsData} />
     </>
   );
 };
