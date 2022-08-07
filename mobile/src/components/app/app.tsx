dddc20c6410e50aeea21c4130afaa000464a7c66
alexandr <1982alexvm@gmail.com>
2022-08-07 21:31:12 +0300

    GRHB-23: + add navigation config and use prop linking to NavigationContainer

--- mobile/src/components/app/app.tsx
+++ mobile/src/components/app/app.tsx
@@ -5,11 +5,16 @@ import 'fast-text-encoding';
 
 import { Root as RootNavigation } from '~/navigation/root/root.navigation';
 import { styles } from './styles';
+import { ENV } from '~/common/enums/enums';
 
 const App: FC = () => {
+  const linking = {
+    prefixes: [ENV.APP.APP_HOST, ENV.APP.APP_SCHEME],
+  };
+
   return (
     <GestureHandlerRootView style={styles.rootView}>
-      <NavigationContainer>
+      <NavigationContainer linking={linking}>
         <RootNavigation />
       </NavigationContainer>
     </GestureHandlerRootView>
