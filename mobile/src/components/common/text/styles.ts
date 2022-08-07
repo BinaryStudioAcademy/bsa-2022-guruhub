198a5082eae7624e40c9f5d20421588e603c9604
Dmitrij Revenets <89215185+revenets@users.noreply.github.com>
2022-08-07 22:37:56 +0300

    Update mobile/src/components/common/text/styles.ts
    Co-authored-by: Liza-Veis <64521658+Liza-Veis@users.noreply.github.com>

--- mobile/src/components/common/text/styles.ts
+++ mobile/src/components/common/text/styles.ts
@@ -3,7 +3,7 @@ import { AppColor } from '~/common/enums/ui/app-color.enum';
 import { AppFontFamily } from '~/common/enums/ui/app-font.enum';
 
 const styles = StyleSheet.create({
-  default: {
+  text: {
     fontFamily: AppFontFamily.INTER_400,
     fontSize: 14,
     color: AppColor.TEXT.GRAY_400,
