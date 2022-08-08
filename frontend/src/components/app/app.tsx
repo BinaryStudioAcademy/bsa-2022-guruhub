5fab54561c9440e3374848a5b6d26018e16220ee
oksana kravets <ksenjakravets@gmail.com>
2022-08-08 17:45:20 +0300

    GRHB-63: * moved wrapping to root component + added wrapper export to common file

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -2,17 +2,20 @@ import { AppRoute } from 'common/enums/enums';
 import { FC } from 'common/types/types';
 import { Routes, Route } from 'components/common/common';
 import { Auth } from 'components/auth/auth';
-import { AuthorizedWrapper } from 'components/common/authorized-wrapper/authorized-wrapper';
+import { AuthorizedWrapper } from 'components/common/common';
 
 const App: FC = () => {
   return (
-    <AuthorizedWrapper>
+    <>
       <Routes>
-        <Route path={AppRoute.ROOT} element="Root" />
+        <Route
+          path={AppRoute.ROOT}
+          element={<AuthorizedWrapper>Root</AuthorizedWrapper>}
+        />
         <Route path={AppRoute.SIGN_UP} element={<Auth />} />
         <Route path={AppRoute.SIGN_IN} element={<Auth />} />
       </Routes>
-    </AuthorizedWrapper>
+    </>
   );
 };
 
