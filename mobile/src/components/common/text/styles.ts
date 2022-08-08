ecc62cdf7e9c66e5c79390dd87bbaf93dc4de1bb
Revenets <revenets@gmail.com>
2022-08-08 10:53:06 +0300

    GRHB-26: * change import

--- mobile/src/components/common/text/styles.ts
+++ mobile/src/components/common/text/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { AppColor } from '~/common/enums/ui/app-color.enum';
-import { AppFontFamily } from '~/common/enums/ui/app-font.enum';
+import { AppFontFamily } from '~/common/enums/ui/app-font-family.enum';
 
 const styles = StyleSheet.create({
   text: {
