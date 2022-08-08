259ef7822888ec05f5afdfa6d9ad983c4d4f4ee4
oksana kravets <ksenjakravets@gmail.com>
2022-08-08 17:53:52 +0300

    GRHB-63: * fixed linter error about duplicated import

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,8 +1,7 @@
 import { AppRoute } from 'common/enums/enums';
 import { FC } from 'common/types/types';
-import { Routes, Route } from 'components/common/common';
+import { Routes, Route, AuthorizedWrapper } from 'components/common/common';
 import { Auth } from 'components/auth/auth';
-import { AuthorizedWrapper } from 'components/common/common';
 
 const App: FC = () => {
   return (
