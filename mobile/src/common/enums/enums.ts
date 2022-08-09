1559577f0766e1b4079c0b02ff43fcdaf95ebda0
Oleksandr Borysenko <3pc@ukr.net>
2022-08-09 18:15:17 +0300

    GRHB-59: - resolve conflict

--- mobile/src/common/enums/enums.ts
+++ mobile/src/common/enums/enums.ts
@@ -3,8 +3,5 @@ export { DataStatus, ENV, StorageKey } from './app/app';
 export { ContentType } from './file/file';
 export { HttpHeader, HttpMethod } from './http/http';
 export { RootScreenName } from './navigation/navigation';
-export {
-  NotificationTitle,
-  NotificationType,
-} from './notification/notification';
+export { NotificationType } from './notification/notification';
 export { AppColor, AppFontFamily } from './ui/ui';
