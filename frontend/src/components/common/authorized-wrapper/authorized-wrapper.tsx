4af0227417ae0e27d7bd456264e40060fb24d4e9
fshabanov <fshabanov01@gmail.com>
2022-08-09 15:30:47 +0400

    GRHB-47: * AuthorizedMenu inside AuthorizedWrapper

--- frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
+++ frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'common/types/types';
 
 import { AuthorizedMenu, Header } from '../common';
+import styles from './styles.module.scss';
 
 const AuthorizedWrapper: FC = ({ children }) => {
   return (
     <>
       <Header />
-      <AuthorizedMenu />
-      {children}
+      <div className={styles.content}>
+        <AuthorizedMenu />
+        {children}
+      </div>
     </>
   );
 };
