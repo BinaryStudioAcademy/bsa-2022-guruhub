355872d70c7e15649b0377590c1acdecd6147eb0
SomMariya <m.som-2030@kpi.ua>
2022-08-09 14:40:19 +0300

    GRHB-45: * PR issues

--- frontend/src/components/common/spinner/spinner.tsx
+++ frontend/src/components/common/spinner/spinner.tsx
@@ -6,13 +6,16 @@ type Props = {
   isOverflow?: boolean;
 };
 
-const Spinner: FC<Props> = ({ isOverflow = false }) =>
-  isOverflow ? (
-    <div className={styles.container}>
-      <div className={styles.loader}></div>
-    </div>
-  ) : (
-    <div className={styles.loader}></div>
-  );
+const Spinner: FC<Props> = ({ isOverflow = false }) => {
+  if (isOverflow) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.loader}></div>
+      </div>
+    );
+  }
+
+  return <div className={styles.loader}></div>;
+};
 
 export { Spinner };
