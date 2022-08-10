c22dc286dd8b2e73d340ae08cf3264dec5012625
Alexandr <1982alexvm@gmail.com>
2022-08-10 19:14:14 +0300

    GRHB-30: + add search icon

--- mobile/src/components/common/search/search.tsx
+++ mobile/src/components/common/search/search.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react';
-import { TextInput } from 'react-native';
+import { ImageURISource, TextInput } from 'react-native';
 
 import { AppColor } from '~/common/enums/enums';
-import { View } from '~/components/common/common';
+import { Image, View } from '~/components/common/common';
 import { useEffect, useState } from '~/hooks/hooks';
 
 import { styles } from './styles';
@@ -10,6 +10,13 @@ import { styles } from './styles';
 type Props = {
   onSearch: (text: string) => void;
 };
+
+type IconSize = {
+  width: number;
+  height: number;
+};
+
+type IconName = 'search' | 'voice';
 
 const Search: FC<Props> = ({ onSearch }) => {
   const [text, setText] = useState('');
@@ -26,8 +33,26 @@ const Search: FC<Props> = ({ onSearch }) => {
     setText(value);
   };
 
+  const getUriSource = (name: IconName): ImageURISource => {
+    if (name === 'search') {
+      return require('../../../assets/img/search.png');
+    }
+
+    return require('../../../assets/img/voice.png');
+  };
+
+  const getIcon = (name: IconName, size: IconSize): JSX.Element => {
+    return (
+      <Image
+        source={Image.resolveAssetSource(getUriSource(name))}
+        style={{ width: size.width, height: size.height }}
+      />
+    );
+  };
+
   return (
     <View style={styles.searchBar}>
+      {getIcon('search', { width: 25, height: 25 })}
       <TextInput
         style={styles.search}
         autoComplete="off"
