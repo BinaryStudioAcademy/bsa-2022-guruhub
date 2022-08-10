7d5ba779f61ed1b9a0d1378932a0d24607fe0459
kalinkaaaa14 <kalinkaaaa14@gmail.com>
2022-08-10 21:24:06 +0300

    GRHB-41: * group error refactored

--- backend/src/exceptions/exceptions.ts
+++ backend/src/exceptions/exceptions.ts
@@ -1,6 +1,6 @@
 export { AuthError } from './auth-error/auth-error.exception';
+export { InvalidGroupError } from './invalid-group-error/invalid-group-error.exception';
 export {
   HttpError,
   InvalidCredentialsError,
-  InvalidGroupError,
 } from 'guruhub-shared/exceptions/exceptions';
--- backend/src/exceptions/invalid-group-error/invalid-group-error.exception.ts
+++ backend/src/exceptions/invalid-group-error/invalid-group-error.exception.ts
@@ -1,18 +1,17 @@
+import { HttpError } from 'guruhub-shared/exceptions/exceptions';
+
 import {
   CustomExceptionName,
   ExceptionMessage,
   HttpCode,
 } from '~/common/enums/enums';
 
-class InvalidGroupError extends Error {
-  status: HttpCode;
-
+class InvalidGroupError extends HttpError {
   constructor({
     message = ExceptionMessage.INVALID_GROUP_NAME,
     status = HttpCode.BAD_REQUEST,
   } = {}) {
-    super(message);
-    this.status = status;
+    super({ message, status });
     this.name = CustomExceptionName.INVALID_GROUP;
   }
 }
