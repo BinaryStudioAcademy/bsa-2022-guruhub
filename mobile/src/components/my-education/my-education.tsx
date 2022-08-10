d86172ac755233ad99ba6d5e249ec2baddc685a6
Yaroslav Lebedenko <nailheart4@gmail.com>
2022-08-10 11:17:16 +0300

    GRHB27: * eslint fix

--- mobile/src/components/my-education/my-education.tsx
+++ mobile/src/components/my-education/my-education.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
-import { Text, View } from 'react-native';
+
+import { Text, View } from '~/components/common/common';
 
 const MyEducation: FC = () => {
   return (
