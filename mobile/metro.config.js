1a17d489514d5d41da910627d8a63663c641834b
Yaroslav Lebedenko <nailheart4@gmail.com>
2022-08-10 12:37:55 +0300

    GRHB-27: * fixed quotes
    Co-authored-by: Volodymyr Minchenko <voldemarprk2001@gmail.com>

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -37,7 +37,7 @@ const configB = (async () => {
       babelTransformerPath: require.resolve("react-native-svg-transformer")
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
       sourceExts: [...sourceExts, "svg"]
     }
   };
