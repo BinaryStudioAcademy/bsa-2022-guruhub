189e2913132d87e2c42bbed08ecfafd1354f47ed
Oleksandr Borysenko <3pc@ukr.net>
2022-08-11 15:24:12 +0300

    GRHB-28: * resolve conflict

--- mobile/src/common/types/courses/course-item.type.ts
+++ mobile/src/common/types/courses/course-item.type.ts
@@ -4,8 +4,6 @@ type CoursesGetAllItemResponseDto = {
   vendor_name: string;
   difficulty: string;
   author_name: string;
-  rating_star: string;
-  ratings_count: number;
   is_bestseller: boolean;
   price: number;
   course_image: string;
