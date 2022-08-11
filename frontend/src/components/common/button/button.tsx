759bef43d2d00672ab9d7675a74f630c8b271b74
Oleksandr Lokhonia <oleksandr_lokhonia@epam.com>
2022-08-11 21:40:48 +0300

    + header log in and sign up buttons when user is not authorized

--- frontend/src/components/common/button/button.tsx
+++ frontend/src/components/common/button/button.tsx
@@ -1,15 +1,25 @@
 import { FC } from 'common/types/types';
+import { getValidClasses } from 'helpers/helpers';
 
 import styles from './styles.module.scss';
 
 type Props = {
   label: string;
+  color?: string;
   type?: 'button' | 'submit';
   onClick?: () => void;
 };
 
-const Button: FC<Props> = ({ type = 'button', label, onClick }) => (
-  <button type={type} className={styles.button} onClick={onClick}>
+const Button: FC<Props> = ({ type = 'button', label, color, onClick }) => (
+  <button
+    type={type}
+    className={getValidClasses(
+      styles.button,
+      color && styles[`button__${color}`],
+    )}
+    color={color}
+    onClick={onClick}
+  >
     {label}
   </button>
 );
--- frontend/src/components/common/header/header.tsx
+++ frontend/src/components/common/header/header.tsx
@@ -1,17 +1,23 @@
 import defaultUserAvatar from 'assets/img/avatar-default.svg';
 import logo from 'assets/img/logo.svg';
+import { AppRoute } from 'common/enums/enums';
 import { FC } from 'common/types/types';
-import { Image } from 'components/common/common';
-import { useState } from 'hooks/hooks';
+import { Button, Image } from 'components/common/common';
+import { useAppSelector, useNavigate, useState } from 'hooks/hooks';
 
 import { Popup } from './components/components';
 import styles from './styles.module.scss';
 
 const Header: FC = () => {
   const [isMenuPopupVisible, setIsMenuPopupVisible] = useState<boolean>(false);
+  const { user } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
 
   const handlePopupOper = (): void =>
     setIsMenuPopupVisible(!isMenuPopupVisible);
+
+  const handleLogIn = (): void => navigate(AppRoute.SIGN_IN);
+  const handleSignUp = (): void => navigate(AppRoute.SIGN_UP);
 
   return (
     <header>
@@ -20,15 +26,22 @@ const Header: FC = () => {
           <Image width="150" height="94" src={logo} alt="logo" />
         </div>
         <div className={styles.userWrapper}>
-          <button onClick={handlePopupOper} className={styles.button}>
-            <Image
-              width="50"
-              height="50"
-              src={defaultUserAvatar}
-              alt="user avatar"
-              isCircular
-            />
-          </button>
+          {!user ? (
+            <div className={styles.buttonsWrapper}>
+              <Button onClick={handleLogIn} label={'Log In'} color={'grey'} />
+              <Button onClick={handleSignUp} label={'Sign Up'} />
+            </div>
+          ) : (
+            <button onClick={handlePopupOper} className={styles.button}>
+              <Image
+                width="50"
+                height="50"
+                src={defaultUserAvatar}
+                alt="user avatar"
+                isCircular
+              />
+            </button>
+          )}
           <div className={styles.popup}>
             {isMenuPopupVisible && <Popup onClose={handlePopupOper} />}
           </div>
