0a13c4f83252db11943cbfebbc099da0baa08fda
Revenets <revenets@gmail.com>
2022-08-11 12:49:54 +0300

    GRHB-32: * remove useless code

--- mobile/src/common/types/navigation/root-navigation-param-list.type.ts
+++ mobile/src/common/types/navigation/root-navigation-param-list.type.ts
@@ -2,8 +2,6 @@ import { RootScreenName } from '~/common/enums/enums';
 
 type RootNavigationParamList = {
   [RootScreenName.AUTH]: undefined;
-  [RootScreenName.SIGN_UP]: undefined;
-  [RootScreenName.SIGN_IN]: undefined;
   [RootScreenName.APP]: undefined;
 };
 
