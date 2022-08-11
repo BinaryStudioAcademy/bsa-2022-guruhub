ebb44e87ce658af47a908a277d3c5d0e42c69edb
oksana kravets <ksenjakravets@gmail.com>
2022-08-11 11:16:16 +0300

    GRHB-63: * overflow to auto and class name to camelcase  - flex-grow for root component

--- frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
+++ frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
@@ -9,7 +9,7 @@ const AuthorizedWrapper: FC = ({ children }) => {
       <Header />
       <div className={styles.content}>
         <AuthorizedMenu />
-        <div className={styles.main_content}>{children}</div>
+        <div className={styles.mainContent}>{children}</div>
       </div>
     </>
   );
