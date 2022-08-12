ee9cfcaee834b1f42c241d167f1a5e581a48c9e7
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-08-12 12:44:02 +0300

    GRHB-104: + dashboard component

---
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import styles from './styles.module.scss';
+
+const Dashboard: FC = () => {
+  return <div className={styles.dashboard}></div>;
+};
+
+export { Dashboard };
