fe01d87558f1be5db8458548e254b9e1c68ac2d8
fshabanov <fshabanov01@gmail.com>
2022-08-12 17:16:12 +0400

    GRHB-131: + courses_to_vendors table

---
+++ backend/src/data/migrations/20220812131517_add_courses_to_vendors_associations.ts
@@ -0,0 +1,35 @@
+import { Knex } from 'knex';
+
+enum TableName {
+  COURSES = 'courses',
+  VENDORS = 'vendors',
+  COURSES_TO_VENDORS = 'courses_to_vendors',
+}
+
+enum ColumnName {
+  ID = 'id',
+  VENDOR_ID = 'vendor_id',
+  COURSE_ID = 'course_id',
+}
+
+async function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable(TableName.COURSES_TO_VENDORS, (table) => {
+    table
+      .integer(ColumnName.VENDOR_ID)
+      .references(ColumnName.ID)
+      .inTable(TableName.VENDORS);
+    table
+      .integer(ColumnName.COURSE_ID)
+      .references(ColumnName.ID)
+      .inTable(TableName.COURSES);
+  });
+}
+
+async function down(knex: Knex): Promise<void> {
+  knex.schema.alterTable(TableName.COURSES_TO_VENDORS, (table) => {
+    table.dropColumn(ColumnName.VENDOR_ID);
+    table.dropColumn(ColumnName.COURSE_ID);
+  });
+}
+
+export { down, up };
