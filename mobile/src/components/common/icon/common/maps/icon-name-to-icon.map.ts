331059868e9123ce33803adaf59b823ea4b35042
Alexandr <1982alexvm@gmail.com>
2022-08-12 11:25:42 +0300

    GRHB-30: + icon-name-to-icon.map.ts

---
+++ mobile/src/components/common/icon/common/maps/icon-name-to-icon.map.ts
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import { SvgProps } from 'react-native-svg';
+
+import Search from '~/assets/icons/search.svg';
+import Voice from '~/assets/icons/voice.svg';
+import { IconName } from '~/common/types/ui/icon-name.type';
+
+const IconNameToIcon: Record<IconName, FC<SvgProps>> = {
+  search: Search,
+  voice: Voice,
+};
+
+export { IconNameToIcon };
