d4f248f01a21673922095ff56d5dbf3248dc4330
fshabanov <fshabanov01@gmail.com>
2022-08-12 17:26:13 +0400

    GRHB-132: + courses_to_course_category table

---
+++ backend/src/data/migrations/20220812132515_add_courses_to_course_category_id_table.ts
@@ -0,0 +1,29 @@
+import { Knex } from 'knex';
+
+const TABLE_NAME = 'courses_to_course_categories';
+
+enum ColumnName {
+  ID = 'id',
+  CREATED_AT = 'created_at',
+  UPDATED_AT = 'updated_at',
+}
+
+async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments(ColumnName.ID).primary();
+    table
+      .dateTime(ColumnName.CREATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .dateTime(ColumnName.UPDATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  });
+}
+
+async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+}
+
+export { down, up };
