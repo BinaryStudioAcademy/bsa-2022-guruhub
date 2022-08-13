a78b64a377119cde6ebdcde1e5b06b8634c4094a
fshabanov <fshabanov01@gmail.com>
2022-08-13 12:00:42 +0400

    GRHB-106: - remove unnecessary table names

--- backend/src/common/enums/db/db-table-name.enum.ts
+++ backend/src/common/enums/db/db-table-name.enum.ts
@@ -4,8 +4,6 @@ enum DbTableName {
   MIGRATIONS = 'migrations',
   COURSE_CATEGORIES = 'course_categories',
   COURSES = 'courses',
-  COURSE_TO_COURSE_CATEGORY = 'course_to_course_categorY',
-  COURSE_TO_VENDOR = 'course_to_vendor',
   GROUPS_TO_PERMISSIONS = 'groups_to_permissions',
   USERS_TO_GROUPS = 'users_to_groups',
   GROUPS = 'groups',
