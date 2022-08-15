fc38096166922d28ee45560e099dadf47b7a2fce
what1s1ove <zubko.2007@gmail.com>
2022-08-15 15:34:07 +0300

    development: * api services

--- frontend/src/services/services.ts
+++ frontend/src/services/services.ts
@@ -1,17 +1,21 @@
 import { ENV } from 'common/enums/enums';
 
 import { AuthApi } from './auth-api/auth-api.service';
-import { CategoriesApi } from './categories/categories.service';
-import { Courses } from './courses/courses.service';
-import { GroupsApi } from './groups/groups.service';
+import { CategoriesApi } from './categories-api/categories-api.service';
+import { Courses } from './courses-api/courses-api.service';
+import { GroupsApi } from './groups-api/groups-api.service';
 import { Http } from './http/http.service';
 import { Notification } from './notification/notification.service';
 import { Storage } from './storage/storage.service';
-import { UsersApi } from './users/users.service';
+import { UsersApi } from './users-api/users-api.service';
 
-const storage = new Storage({ storage: localStorage });
+const storage = new Storage({
+  storage: localStorage,
+});
 
-const http = new Http({ storage });
+const http = new Http({
+  storage,
+});
 
 const authApi = new AuthApi({
   apiPrefix: ENV.API_PATH,
