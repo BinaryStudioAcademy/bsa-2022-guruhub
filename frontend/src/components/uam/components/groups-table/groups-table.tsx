f9996e9f20c804498cfe663cbbd9075e6e63135f
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-08-15 12:44:47 +0300

    GRHB-51: + add group button to group create page

--- frontend/src/components/uam/components/groups-table/groups-table.tsx
+++ frontend/src/components/uam/components/groups-table/groups-table.tsx
@@ -1,5 +1,7 @@
+import { AppRoute } from 'common/enums/enums';
 import { FC, GroupsItemResponseDto } from 'common/types/types';
-import { Table } from 'components/common/common';
+import { Button, Table } from 'components/common/common';
+import { useNavigate } from 'hooks/hooks';
 import { Column } from 'react-table';
 
 import styles from './styles.module.scss';
@@ -10,9 +12,18 @@ type Props = {
 };
 
 const GroupsTable: FC<Props> = ({ columns, data }: Props) => {
+  const navigate = useNavigate();
+
+  const onAddGroupClick = (): void => {
+    navigate(AppRoute.UAM_CREATE_GROUP);
+  };
+
   return (
     <div className={styles.groupsTable}>
-      <h1 className={styles.groupsTableHeading}>Groups</h1>
+      <div className={styles.groupTableHeadingPanel}>
+        <h1 className={styles.groupsTableHeading}>Groups</h1>
+        <Button label="Add Group" type="button" onClick={onAddGroupClick} />
+      </div>
       <Table data={data} columns={columns} />
     </div>
   );
