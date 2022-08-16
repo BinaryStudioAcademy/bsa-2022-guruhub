3c9bc552477a61e4b2a069070ac9a0df64aea0f5
Yelyzaveta Veis <liza.veis14@gmail.com>
2022-08-16 11:40:25 +0200

    development: * imports

--- mobile/src/services/services.ts
+++ mobile/src/services/services.ts
@@ -1,6 +1,7 @@
+import { MMKV } from 'react-native-mmkv';
+
 import { ENV } from '~/common/enums/enums';
 
-import { MMKV } from '../components/common/common';
 import { AuthApi } from './auth-api/auth-api.service';
 import { GroupsApi } from './groups-api/groups-api.service';
 import { Http } from './http/http.service';
