79562003b5555758e72aae8b860829c8724890a1
fshabanov <fshabanov01@gmail.com>
2022-08-16 20:20:40 +0400

    GRHB-155: * fix courses table image_url

--- backend/src/data/migrations/20220816160628_courses_table_image_url_fix.ts
+++ backend/src/data/migrations/20220816160628_courses_table_image_url_fix.ts
@@ -6,13 +6,13 @@ const COLUMN_NAME = 'image_url';
 
 async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable(TABLE_NAME, (table) => {
-    table.string(COLUMN_NAME).nullable();
+    table.setNullable(COLUMN_NAME);
   });
 }
 
 async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable(TABLE_NAME, (table) => {
-    table.dropColumn(COLUMN_NAME);
+    table.string(COLUMN_NAME).notNullable().alter();
   });
 }
 
