94ca7e9e04b12a3036198bf2a4b68923780d54ba
Revenets <revenets@gmail.com>
2022-08-18 15:20:05 +0300

    GRHB-144: * rename folder

--- mobile/src/store/root-reducer.ts
+++ mobile/src/store/root-reducer.ts
@@ -1,6 +1,6 @@
 import { reducer as auth } from './auth/reducer';
 import { reducer as uam } from './uam/reducer';
-import { reducer as uamGroupConfig } from './uam-group-config/reducer';
+import { reducer as uamGroupConfig } from './uam-group-edit/reducer';
 import { reducer as uamGroupCreation } from './uam-groups-create/reducer';
 
 const rootReducer = {
