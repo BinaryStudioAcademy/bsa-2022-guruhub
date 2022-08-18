69c17e1c970410945a2f842a9835cd8016c334d2
Revenets <revenets@gmail.com>
2022-08-18 21:13:57 +0300

    GRHB-144: * update validation schemas

--- shared/src/validation-schemas/validation-schemas.ts
+++ shared/src/validation-schemas/validation-schemas.ts
@@ -1,4 +1,10 @@
 export { courseCreate, courseFiltering } from './course/course';
-export { groupCreate, groupCreateClient, groupDelete } from './group/group';
+export {
+  groupCreate,
+  groupCreateClient,
+  groupDelete,
+  groupUpdate,
+  groupUpdateParams,
+} from './group/group';
 export { pagination } from './pagination/pagination';
 export { userDelete, userSignIn, userSignUp } from './user/user';
