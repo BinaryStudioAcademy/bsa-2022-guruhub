e42221961ab8a422a93d1b659f67df3ddede6a9c
fshabanov <fshabanov01@gmail.com>
2022-08-18 23:22:47 +0400

    GRHB164: * fix common import

--- backend/src/common/constants/api.constants.ts
+++ backend/src/common/constants/api.constants.ts
@@ -4,7 +4,7 @@ import {
   CategoriesApiPath,
   CoursesApiPath,
   ENV,
-} from '../enums/enums';
+} from '~/common/enums/enums';
 
 const WHITE_ROUTES = [
   `${ENV.API.V1_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_IN}`,
