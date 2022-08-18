64763ae540cc3ccc844814e65f57cf3ab8882176
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-08-19 01:08:23 +0300

    GRHB-167: + migration mostly done bug with migration

--- backend/src/data/migrations/20220818205843_add_admin_group_and_administrator.ts
+++ backend/src/data/migrations/20220818205843_add_admin_group_and_administrator.ts
@@ -1,27 +1,55 @@
 import { Knex } from 'knex';
 
-const adminDbCridentials = {
+import {
+  GroupsCreateRequestDto,
+  UserSignUpRequestDto,
+} from '~/common/types/types';
+import { auth, group } from '~/services/services';
+
+const adminCridentials: UserSignUpRequestDto = {
   email: 'admin@guruhub.club',
   fullName: 'Admin',
-  passwordSalt: '$2b$10$Dq7FKk/k/nhhPwodL6V3G.',
-  passwordHash: '$2b$10$Dq7FKk/k/nhhPwodL6V3G.DRTSUnsCMTKocaCTGvU4rl9WYP.8g3e',
+  password: 'Pa55word',
 };
+
+const adminGroupName = 'Admins';
 
 const TableName = {
   USERS: 'users',
+  PERMISSIONS: 'permissions',
+  USERS_TO_GROUPS: 'users_to_groups',
+  GROUPS: 'groups',
+  GROUPS_TO_PERMISSIONS: 'groups_to_permissions',
 };
 
 async function up(knex: Knex): Promise<void> {
-  await knex(TableName.USERS).insert(adminDbCridentials);
+  await auth.signUp(adminCridentials);
 
   const adminId = await knex(TableName.USERS)
     .select('id')
-    .where('email', adminDbCridentials.email);
-  JSON.stringify(adminId);
+    .where('email', adminCridentials.email);
+
+  const idToGivePermissions = adminId[0].id;
+
+  if (adminId.length === 1 && idToGivePermissions) {
+    const permissionIds = await knex(TableName.PERMISSIONS).select('id');
+    const premissionIdsToGive = permissionIds.map(
+      (permission) => permission.id,
+    );
+
+    const adminPermissionsDto: GroupsCreateRequestDto = {
+      name: adminGroupName,
+      permissionIds: premissionIdsToGive,
+      userIds: [idToGivePermissions],
+    };
+
+    await group.create(adminPermissionsDto);
+  }
 }
 
 async function down(knex: Knex): Promise<void> {
-  await knex(TableName.USERS).where('email', adminDbCridentials.email).del();
+  await knex(TableName.USERS).where('email', adminCridentials.email).del();
+  await knex(TableName.GROUPS).where('name', adminGroupName).del();
 }
 
 export { down, up };
