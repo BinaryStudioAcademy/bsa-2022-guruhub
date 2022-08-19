ff177536389ff15cc1ad9f77353d569676ede0d5
Alexandr <1982alexvm@gmail.com>
2022-08-19 14:32:31 +0300

    GRHB-197: + Add padding to settings and uam screens

--- mobile/src/components/uam/uam.tsx
+++ mobile/src/components/uam/uam.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 
 import { AppScreenName } from '~/common/enums/enums';
-import { ScrollView } from '~/components/common/common';
+import { ScrollView, View } from '~/components/common/common';
 import { useAppNavigate } from '~/hooks/hooks';
 
 import { Button } from '../common/common';
 import { GroupsTable } from './groups-table/groups-table';
+import { styles } from './styles';
 import { UsersTable } from './users-table/users-table';
 
 const UAM: FC = () => {
@@ -16,11 +17,13 @@ const UAM: FC = () => {
   };
 
   return (
-    <ScrollView>
-      <UsersTable />
-      <GroupsTable />
-      <Button label="Create group" onPress={handleGroupCreate} />
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView>
+        <UsersTable />
+        <GroupsTable />
+        <Button label="Create group" onPress={handleGroupCreate} />
+      </ScrollView>
+    </View>
   );
 };
 
