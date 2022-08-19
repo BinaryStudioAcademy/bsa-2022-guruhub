926d9e95293c9eb98b1a705f4ced3204abf6682f
Oleksandr Borysenko <3pc@ukr.net>
2022-08-19 15:33:14 +0300

    GRHB-28: - delete assets reletad with coursera vendor

--- mobile/src/components/courses/components/course-card/common/maps/vendor-key-to-image.map.ts
+++ mobile/src/components/courses/components/course-card/common/maps/vendor-key-to-image.map.ts
@@ -1,12 +1,10 @@
 import { FC } from 'react';
 import { SvgProps } from 'react-native-svg';
 
-import Coursera from '~/assets/images/vendors/coursera.svg';
 import Udemy from '~/assets/images/vendors/udemy.svg';
 
 const vendorsKeyToImage: Record<string, FC<SvgProps>> = {
   udemy: Udemy,
-  coursera: Coursera,
 };
 
 export { vendorsKeyToImage };
