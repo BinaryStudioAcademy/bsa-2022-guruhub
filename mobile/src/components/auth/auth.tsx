b73b89cf8e1fca95916bddc6399a52d4866dcb90
Oleksandr Borysenko <3pc@ukr.net>
2022-08-19 16:21:04 +0300

    GRHB-196: * resolve conflict

--- mobile/src/components/auth/auth.tsx
+++ mobile/src/components/auth/auth.tsx
@@ -44,7 +44,7 @@ const Auth: FC = () => {
       <Image source={{ uri: getImageUri(logo) }} style={styles.logo} />
       <View style={styles.circle1} />
       <View style={styles.circle2} />
-      <ScrollView keyboardShouldPersistTaps="handled">
+      <ScrollView keyboardShouldPersistTaps="never">
         {getScreen(name)}
       </ScrollView>
     </View>
