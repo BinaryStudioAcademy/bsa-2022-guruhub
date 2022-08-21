5514fc733f7b59401aeb5c6c3f83d2aaf9ede776
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-08-21 10:58:35 +0300

    GRHB-177: * fixed onClose function type

--- frontend/src/components/common/modal/modal.tsx
+++ frontend/src/components/common/modal/modal.tsx
@@ -7,7 +7,7 @@ import styles from './styles.module.scss';
 
 type Props = {
   isOpen: boolean;
-  onClose: (e: React.MouseEvent) => void;
+  onClose: (e?: React.MouseEvent) => void;
   title: string;
   children: ReactNode;
 };
