f75a895df3ef09c41cb0bc57beadd973090d223c
Revenets <revenets@gmail.com>
2022-08-22 17:03:12 +0300

    GRHB-244: * make edit handler async

--- mobile/src/components/uam/groups-table/groups-table.tsx
+++ mobile/src/components/uam/groups-table/groups-table.tsx
@@ -26,8 +26,8 @@ const GroupsTable: FC = () => {
     dispatch(uamActions.deleteGroup({ id: groupId }));
   };
 
-  const handleGroupsItemEdit = (groupId: number): void => {
-    dispatch(uamGroupEditActions.getGroupById({ id: groupId }));
+  const handleGroupsItemEdit = async (groupId: number): Promise<void> => {
+    await dispatch(uamGroupEditActions.getGroupById({ id: groupId }));
     navigation.navigate(AppScreenName.UAM_GROUPS_EDIT);
   };
 
