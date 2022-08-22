cb0274983e95cf71658f8dbde8343aa51f5859d2
SomMariya <m.som-2030@kpi.ua>
2022-08-22 20:37:33 +0300

    GRHB-247: + added auth check for side menu

--- frontend/src/components/common/authorized-menu/authorized-menu.tsx
+++ frontend/src/components/common/authorized-menu/authorized-menu.tsx
@@ -1,10 +1,18 @@
 import { FC } from 'common/types/types';
+import { useAppSelector } from 'hooks/hooks';
 
 import { routes } from './common';
 import { NavigationMenu } from './components/components';
 import styles from './styles.module.scss';
 
 const AuthorizedMenu: FC = () => {
+  const { user } = useAppSelector((state) => state.auth);
+  const hasUser = Boolean(user);
+
+  if (!hasUser) {
+    return <></>;
+  }
+
   return (
     <div className={styles.menu}>
       {routes.map(({ name, subroutes }) => (
