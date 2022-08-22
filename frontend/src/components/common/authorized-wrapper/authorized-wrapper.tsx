f295b43b090ff8da73f4cb1c4d16dbedf3df01ab
SomMariya <m.som-2030@kpi.ua>
2022-08-22 20:57:05 +0300

    GRHB-247: * moved auth check to AuthorizedWrapper

--- frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
+++ frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
@@ -1,9 +1,24 @@
 import { FC } from 'common/types/types';
+import { useAppSelector } from 'hooks/hooks';
 
 import { AuthorizedMenu, Header } from '../common';
 import styles from './styles.module.scss';
 
 const AuthorizedWrapper: FC = ({ children }) => {
+  const { user } = useAppSelector((state) => state.auth);
+  const hasUser = Boolean(user);
+
+  if (!hasUser) {
+    return (
+      <>
+        <Header />
+        <div className={styles.content}>
+          <div className={styles.mainContent}>{children}</div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Header />
