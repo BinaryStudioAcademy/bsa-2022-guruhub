62d509437c231a5bf5334762cda0ec8a44716a46
SomMariya <m.som-2030@kpi.ua>
2022-08-22 21:07:55 +0300

    GRHB-247: simplified check

--- frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
+++ frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
@@ -8,22 +8,11 @@ const AuthorizedWrapper: FC = ({ children }) => {
   const { user } = useAppSelector((state) => state.auth);
   const hasUser = Boolean(user);
 
-  if (!hasUser) {
-    return (
-      <>
-        <Header />
-        <div className={styles.content}>
-          <div className={styles.mainContent}>{children}</div>
-        </div>
-      </>
-    );
-  }
-
   return (
     <>
       <Header />
       <div className={styles.content}>
-        <AuthorizedMenu />
+        {hasUser && <AuthorizedMenu />}
         <div className={styles.mainContent}>{children}</div>
       </div>
     </>
