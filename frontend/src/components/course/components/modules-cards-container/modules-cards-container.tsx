680b0134753b3d730aed3d6cc7af8c1577aa7c51
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-08-23 13:32:33 +0300

    GRHB-246: * modules cards are now links to module page

--- frontend/src/components/course/components/modules-cards-container/modules-cards-container.tsx
+++ frontend/src/components/course/components/modules-cards-container/modules-cards-container.tsx
@@ -1,4 +1,7 @@
+import { AppRoute } from 'common/enums/enums';
 import { CourseModulesGetAllItemResponseDto, FC } from 'common/types/types';
+import { Link } from 'components/common/common';
+import { useAppSelector } from 'hooks/hooks';
 
 import { ModuleCard } from './components/module-card/module-card';
 import styles from './styles.module.scss';
@@ -8,16 +11,25 @@ type Props = {
 };
 
 const ModulesCardsContainer: FC<Props> = ({ modules }) => {
+  const { course } = useAppSelector((state) => state.course);
+
   return (
     <ul className={styles.container}>
       {modules.map((courseModule) => {
         return (
-          <li key={courseModule.id} className={styles.moduleCardContainer}>
-            <ModuleCard
-              title={courseModule.title}
-              description={courseModule.description}
-            />
-          </li>
+          <Link
+            to={
+              `${AppRoute.COURSES}/${course?.id}/modules/${courseModule.id}` as AppRoute
+            }
+            className={styles.linkToModule}
+          >
+            <li key={courseModule.id} className={styles.moduleCardContainer}>
+              <ModuleCard
+                title={courseModule.title}
+                description={courseModule.description}
+              />
+            </li>
+          </Link>
         );
       })}
     </ul>
