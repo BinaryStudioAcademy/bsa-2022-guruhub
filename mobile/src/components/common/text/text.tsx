4fe77c4ea2eb916889785bf55da93a4a85c86cef
Yaroslav Lebedenko <nailheart4@gmail.com>
2022-08-23 21:09:17 +0300

    GRHB-232: * add TextProps to Text component

--- mobile/src/components/common/text/text.tsx
+++ mobile/src/components/common/text/text.tsx
@@ -1,17 +1,17 @@
 import React, { FC } from 'react';
-import { Text as UIText } from 'react-native';
+import { Text as UIText, TextProps } from 'react-native';
 
 import { AppTextStyle } from '~/common/types/types';
 
 import { styles } from './styles';
 
-type Props = {
+type Props = TextProps & {
   children: string;
   style?: AppTextStyle;
 };
 
 const Text: FC<Props> = ({ style, children }) => {
-  return <UIText style={{ ...styles.text, ...style }}>{children}</UIText>;
+  return <UIText style={[styles.text, style]}>{children}</UIText>;
 };
 
 export { Text };
