40dad892623d869823196727e0f723de2d623d9f
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-08-24 18:42:07 +0300

    GRHB-269: + basic categories markup

--- frontend/src/components/course/components/add-categories-modal/add-categories-modal.tsx
+++ frontend/src/components/course/components/add-categories-modal/add-categories-modal.tsx
@@ -1,48 +1,67 @@
-// import { CourseUpdateCategoryRequestDto, FC } from 'common/types/types';
-// import { Button, Input, Modal } from 'components/common/common';
-// import { getNameOf } from 'helpers/helpers';
-// import { useAppDispatch, useAppForm } from 'hooks/hooks';
+import {
+  CategoryGetAllItemResponseDto,
+  CourseUpdateCategoryRequestDto,
+  FC,
+} from 'common/types/types';
+import { Button, Modal, Select } from 'components/common/common';
+import { getNameOf } from 'helpers/helpers';
+import { useAppForm } from 'hooks/hooks';
 // import { courseActions } from 'store/actions';
-// import { courseCreate as courseCreateValidationSchema } from 'validation-schemas/validation-schemas';
+import { courseCreate as courseCreateValidationSchema } from 'validation-schemas/validation-schemas';
 
-// import styles from './styles.module.scss';
+import { getDefaultUpdateCourseCategoryPayload } from './common';
+import { getCategoriesOptions } from './helpers/helpers';
+import styles from './styles.module.scss';
 
-// type Props = {
-//   isModalOpen: boolean;
-//   onModalToggle: () => void;
-// };
+type Props = {
+  categories: CategoryGetAllItemResponseDto[];
+  defaultId: number;
+  isModalOpen: boolean;
+  onModalToggle: () => void;
+};
 
-// const AddCategoriesModal: FC<Props> = ({ isModalOpen, onModalToggle }) => {
-//   const { control, errors, handleSubmit } = useAppForm<CourseUpdateCategoryRequestDto>({
-//     defaultValues: DEFAULT_CREATE_COURSE_PAYLOAD,
-//     validationSchema: courseCreateValidationSchema,
-//   });
+const AddCategoriesModal: FC<Props> = ({
+  categories,
+  defaultId,
+  isModalOpen,
+  onModalToggle,
+}) => {
+  // const dispatch = useAppDispatch();
 
-//   const dispatch = useAppDispatch();
+  const { control, errors, handleSubmit } =
+    useAppForm<CourseUpdateCategoryRequestDto>({
+      defaultValues: getDefaultUpdateCourseCategoryPayload(defaultId),
+      validationSchema: courseCreateValidationSchema,
+    });
 
-//   const onSubmit = (payload: CourseUpdateCategoryRequestDto): void => {
-//     dispatch(dashboardActions.addCourse(payload)).unwrap().then(onModalToggle);
-//   };
+  const categoriesOptions = getCategoriesOptions(categories);
 
-//   return (
-//     <Modal isOpen={isModalOpen} onClose={onModalToggle} title="Select new category">
-//       <h3>Import</h3>
-//       <form onSubmit={handleSubmit(onSubmit)} className={styles.formWrapper}>
-//         <div className={styles.formContent}>
-//           <Input
-//             type="text"
-//             label="Place the link"
-//             name={getNameOf<CourseUpdateCategoryRequestDto>('url')}
-//             control={control}
-//             errors={errors}
-//           />
-//         </div>
-//         <div className={styles.buttonWrapper}>
-//           <Button type="submit" label="Submit course" btnColor="blue" />
-//         </div>
-//       </form>
-//     </Modal>
-//   );
-// };
+  const onSubmit = (payload: CourseUpdateCategoryRequestDto): void => {
+    console.log(payload);
+  };
 
-// export { AddCategoriesModal };
+  return (
+    <Modal
+      isOpen={isModalOpen}
+      onClose={onModalToggle}
+      title="Select new category"
+    >
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.formWrapper}>
+        <div className={styles.formContent}>
+          <Select
+            options={categoriesOptions}
+            control={control}
+            errors={errors}
+            name={getNameOf<CourseUpdateCategoryRequestDto>('newCategoryId')}
+            label="Course categories"
+          />
+        </div>
+        <div className={styles.buttonWrapper}>
+          <Button type="submit" label="Edit" btnColor="blue" />
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export { AddCategoriesModal };
---
+++ frontend/src/components/course/components/add-categories-modal/helpers/get-categories-options-helper/get-categories-options.helper.ts
@@ -0,0 +1,15 @@
+import {
+  CategoryGetAllItemResponseDto,
+  SelectorOptions,
+} from 'common/types/types';
+
+const getCategoriesOptions = (
+  categories: CategoryGetAllItemResponseDto[],
+): SelectorOptions[] => {
+  return categories.map((category) => ({
+    name: category.name,
+    value: `${category.id}`,
+  }));
+};
+
+export { getCategoriesOptions };
