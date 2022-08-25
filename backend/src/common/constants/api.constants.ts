4a1600bbf11ed2e99c95ea830b5a7497477eee59
fshabanov <fshabanov01@gmail.com>
2022-08-25 09:50:22 +0400

    GRHB-252: + white routes

--- backend/src/common/constants/api.constants.ts
+++ backend/src/common/constants/api.constants.ts
@@ -4,6 +4,8 @@ import {
   CategoriesApiPath,
   CoursesApiPath,
   ENV,
+  InterviewsApiPath,
+  MentorsApiPath,
 } from '~/common/enums/enums';
 
 const WHITE_ROUTES = [
@@ -11,6 +13,8 @@ const WHITE_ROUTES = [
   `${ENV.API.V1_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`,
   `${ENV.API.V1_PREFIX}${ApiPath.CATEGORIES}${CategoriesApiPath.ROOT}`,
   `${ENV.API.V1_PREFIX}${ApiPath.COURSES}${CoursesApiPath.ROOT}`,
+  `${ENV.API.V1_PREFIX}${ApiPath.INTERVIEWS}${InterviewsApiPath.INTERVIEWEE_USER_$ID_CATEGORIES}`,
+  `${ENV.API.V1_PREFIX}${ApiPath.MENTORS}${MentorsApiPath.ROOT}`,
 ];
 
 export { WHITE_ROUTES };
