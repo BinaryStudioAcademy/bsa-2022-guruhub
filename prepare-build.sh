ddbc1bb46062cc40136d0a142e0bcf8429bef195
R-Mielamud <2m.roman2@gmail.com>
2022-08-26 17:22:13 +0300

    GRHB-274: * changed moving backend build folder to copying, removed filling env files, added seeding

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ./build/backend/; mv ./backend/build/* ./build/backend/
+mkdir -p ./build/backend/; cp ./backend/build/* ./build/backend/
 mkdir ./build/shared/; mv ./shared/build ./build/shared/
 mkdir ./build/backend/public; mv ./frontend/build/* ./build/backend/public
 cp package.json package-lock.json ./build
