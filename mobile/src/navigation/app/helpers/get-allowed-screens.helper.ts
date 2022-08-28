ff007c48f7c1f7d297ffc498a69c5819e695cc39
alexandr <1982alexvm@gmail.com>
2022-08-28 21:41:09 +0300

    GRHB-266: * use checkHasPermissions from shared

--- mobile/src/navigation/app/helpers/get-allowed-screens.helper.ts
+++ mobile/src/navigation/app/helpers/get-allowed-screens.helper.ts
@@ -2,22 +2,17 @@ import {
   DrawerNavigationItem,
   PermissionsGetAllItemResponseDto,
 } from '~/common/types/types';
+import { checkHasPermission } from '~/helpers/helpers';
 
 const getAllowedScreens = (
   screens: DrawerNavigationItem[],
   userPermissions: PermissionsGetAllItemResponseDto[],
 ): DrawerNavigationItem[] => {
-  const userPermissionKeys = userPermissions.map((item) => item.key);
-
   return screens.filter((screen) => {
-    const isNotRequiringPermissions = !screen.permissions.length;
-
-    return (
-      isNotRequiringPermissions ||
-      screen.permissions.some((permission) =>
-        userPermissionKeys.includes(permission),
-      )
-    );
+    return checkHasPermission({
+      permissionKeys: screen.permissions,
+      userPermissions: userPermissions,
+    });
   });
 };
 
