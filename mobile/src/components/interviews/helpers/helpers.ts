f6ccf635e9933b894d7777626f7fea35ca07b675
Alexandr <1982alexvm@gmail.com>
2022-08-29 17:53:34 +0300

    GRHB-266: - getInterviewsRows helper

--- mobile/src/components/interviews/helpers/helpers.ts
+++ mobile/src/components/interviews/helpers/helpers.ts
@@ -1,2 +1,1 @@
 export { getInterviewsColumns } from './get-interviews-columns-helper/get-interviews-columns.helper';
-export { getInterviewsRows } from './get-interviews-rows-helper/get-interviews-rows.helper';
--- mobile/src/components/interviews/interviews.tsx
+++ mobile/src/components/interviews/interviews.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 
 import { DataStatus } from '~/common/enums/enums';
+import { InterviewsGetAllItemResponseDto } from '~/common/types/types';
 import {
   Pagination,
   ScrollView,
@@ -8,7 +9,12 @@ import {
   View,
 } from '~/components/common/common';
 import { DEFAULT_PAGE_SIZE } from '~/components/interviews/common/constants/constants';
-import { getInterviewsRows } from '~/components/interviews/helpers/helpers';
+import { statusToColor } from '~/components/interviews/common/maps/maps';
+import {
+  CategoryCell,
+  StatusCell,
+} from '~/components/interviews/interviews-table/components/components';
+import { getFormattedDate } from '~/helpers/helpers';
 import {
   useAppDispatch,
   useAppSelector,
@@ -27,7 +33,21 @@ const Interviews: FC = () => {
     useAppSelector((state) => state.interviews);
 
   const { page, handlePageChange } = usePagination();
-  const interviewsRows = getInterviewsRows(interviews);
+
+  const interviewsRows = interviews.map(
+    (item: InterviewsGetAllItemResponseDto) => {
+      return {
+        id: item.id,
+        name: item.interviewee.userDetails.fullName,
+        category: <CategoryCell category={item.courseCategory} />,
+        status: (
+          <StatusCell text={item.status} color={statusToColor[item.status]} />
+        ),
+        interviewer: item.interviewer?.userDetails.fullName || '',
+        date: getFormattedDate(item.interviewDate, 'kk:mm, dd/MM/yyyy'),
+      };
+    },
+  );
 
   const isInterviewsLoading = interviewsDataStatus === DataStatus.PENDING;
 
