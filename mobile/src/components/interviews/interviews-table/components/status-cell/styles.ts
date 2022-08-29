889e17395b47d27253428ebfbbfcc4246720f361
alexandr <1982alexvm@gmail.com>
2022-08-30 00:16:34 +0300

    GRHB-266: * change padding between rows

--- mobile/src/components/interviews/interviews-table/components/status-cell/styles.ts
+++ mobile/src/components/interviews/interviews-table/components/status-cell/styles.ts
@@ -4,14 +4,13 @@ import { AppColor } from '~/common/enums/enums';
 
 const styles = StyleSheet.create({
   statusCellWrapper: {
-    paddingVertical: 10,
+    paddingVertical: 15,
   },
   statusCell: {
     color: AppColor.TEXT.GRAY_100,
     paddingHorizontal: 8,
     marginHorizontal: 24,
     paddingVertical: 3,
-    marginVertical: 3,
     borderRadius: 16,
     borderWidth: 1,
     textAlign: 'center',
