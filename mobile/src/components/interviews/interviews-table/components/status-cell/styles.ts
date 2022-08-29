2099016e411c966ed6d911de7b635b774102ca3b
Alexandr <1982alexvm@gmail.com>
2022-08-29 18:25:20 +0300

    GRHB-266: * change padding in the cells

--- mobile/src/components/interviews/interviews-table/components/status-cell/styles.ts
+++ mobile/src/components/interviews/interviews-table/components/status-cell/styles.ts
@@ -5,10 +5,10 @@ import { AppColor } from '~/common/enums/enums';
 const styles = StyleSheet.create({
   statusCell: {
     color: AppColor.TEXT.GRAY_100,
-    paddingHorizontal: 15,
-    marginHorizontal: 15,
-    paddingVertical: 3,
-    marginVertical: 3,
+    paddingHorizontal: 8,
+    marginHorizontal: 24,
+    paddingVertical: 7,
+    marginVertical: 5,
     borderRadius: 16,
     borderWidth: 1,
     textAlign: 'center',
