9162712e73802e6e189177b975bcc6e9ee9eecd7
fshabanov <fshabanov01@gmail.com>
2022-08-30 14:26:42 +0400

    GRHB-346: + tasks and task_notes tables

---
+++ backend/src/data/migrations/20220830101405_add_task_notes_table.ts
@@ -0,0 +1,45 @@
+import { Knex } from 'knex';
+
+enum TableName {
+  TASKS = 'tasks',
+  TASK_NOTES = 'task_notes',
+  USERS = 'users',
+}
+
+enum ColumnName {
+  ID = 'id',
+  CREATED_AT = 'created_at',
+  UPDATED_AT = 'updated_at',
+  TASK_ID = 'task_id',
+  AUTHOR_ID = 'author_id',
+}
+
+async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TableName.TASK_NOTES, (table) => {
+    table.increments(ColumnName.ID).primary();
+    table
+      .dateTime(ColumnName.CREATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .dateTime(ColumnName.UPDATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .integer(ColumnName.AUTHOR_ID)
+      .references(ColumnName.ID)
+      .inTable(TableName.USERS)
+      .notNullable();
+    table
+      .integer(ColumnName.TASK_ID)
+      .references(ColumnName.ID)
+      .inTable(TableName.TASKS)
+      .notNullable();
+  });
+}
+
+async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(TableName.TASK_NOTES);
+}
+
+export { down, up };
