455752c9d1eba1e35155465eb1858f628aeb1b92
Alina Kupchyk <kalinkaaaa14@gmail.com>
2022-08-31 22:24:49 +0300

    GRHB-353: * fixed api/api

--- backend/src/api/chats/chats.api.ts
+++ backend/src/api/chats/chats.api.ts
@@ -1,15 +1,7 @@
-import { FastifyPluginAsync, FastifyRequest } from 'fastify';
+import { FastifyPluginAsync } from 'fastify';
 
 import { ChatsApiPath, HttpCode, HttpMethod } from '~/common/enums/enums';
-import {
-  ChatMessageCreateRequestParamsDto,
-  ChatMessageGetAllRequestParamsDto,
-} from '~/common/types/types';
 import { chatMessage as chatMessageService } from '~/services/services';
-import {
-  chatMessageCreateArguments as chatMessageCreateArgumentsValidationSchema,
-  chatMessageGetAll as chatMessageGetAllValidationSchema,
-} from '~/validation-schemas/validation-schemas';
 
 type Options = {
   services: {
@@ -25,51 +17,9 @@ const initChatsApi: FastifyPluginAsync<Options> = async (fastify, opts) => {
     url: ChatsApiPath.ROOT,
     async handler(req, rep) {
       const { id } = req.user;
-      const allChatsLastMessagesMessagesDto =
-        await chatMessageService.getAllLastMessages(id);
-
-      return rep.status(HttpCode.OK).send(allChatsLastMessagesMessagesDto);
-    },
-  });
-
-  fastify.route({
-    method: HttpMethod.GET,
-    url: ChatsApiPath.$ID,
-    schema: {
-      params: chatMessageGetAllValidationSchema,
-    },
-    async handler(
-      req: FastifyRequest<{ Params: ChatMessageGetAllRequestParamsDto }>,
-      rep,
-    ) {
-      const { id: userId } = req.user;
-      const { id: chatOpponentId } = req.params;
-      const chatMessagesDto = await chatMessageService.getAll({
-        userId,
-        chatOpponentId,
-      });
+      const chatMessagesDto = await chatMessageService.getAllLastMessages(id);
 
       return rep.status(HttpCode.OK).send(chatMessagesDto);
-    },
-  });
-
-  fastify.route({
-    method: HttpMethod.POST,
-    url: ChatsApiPath.ROOT,
-    schema: { body: chatMessageCreateArgumentsValidationSchema },
-    async handler(
-      req: FastifyRequest<{ Body: ChatMessageCreateRequestParamsDto }>,
-      rep,
-    ) {
-      const { id: userId } = req.user;
-      const { message, chatOpponentId } = req.body;
-      const newChatMessage = await chatMessageService.create({
-        senderId: userId,
-        receiverId: chatOpponentId,
-        message,
-      });
-
-      return rep.status(HttpCode.CREATED).send(newChatMessage);
     },
   });
 };
