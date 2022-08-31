5e9b4a53c738578ccb5337b44d47a406ffd68580
Revenets <revenets@gmail.com>
2022-08-31 19:21:52 +0300

    GRHB-356: * add services

--- mobile/src/services/interviews-api/interviews-api.service.ts
+++ mobile/src/services/interviews-api/interviews-api.service.ts
@@ -1,8 +1,15 @@
-import { ApiPath, HttpMethod, InterviewsApiPath } from '~/common/enums/enums';
+import {
+  ApiPath,
+  ContentType,
+  HttpMethod,
+  InterviewsApiPath,
+} from '~/common/enums/enums';
 import {
   EntityPagination,
   EntityPaginationRequestQueryDto,
   InterviewsGetAllItemResponseDto,
+  InterviewsGetInterviewerResponseDto,
+  InterviewUpdateRequestArgumentsDto,
 } from '~/common/types/types';
 import { Http } from '~/services/http/http.service';
 
@@ -35,6 +42,42 @@ class InterviewsApi {
           page,
           count,
         },
+      },
+    );
+  }
+
+  public getById(id: number): Promise<InterviewsGetAllItemResponseDto> {
+    return this.#http.load(
+      `${this.#apiPrefix}${ApiPath.INTERVIEWS}${InterviewsApiPath.ROOT}${id}`,
+      {
+        method: HttpMethod.GET,
+      },
+    );
+  }
+
+  public getInterviewersByCategory(
+    categoryId: number,
+  ): Promise<InterviewsGetInterviewerResponseDto[]> {
+    return this.#http.load(
+      `${this.#apiPrefix}${ApiPath.INTERVIEWS}${
+        InterviewsApiPath.INTERVIEWERS
+      }${InterviewsApiPath.CATEGORIES}${InterviewsApiPath.ROOT}${categoryId}`,
+      {
+        method: HttpMethod.GET,
+      },
+    );
+  }
+
+  public update({
+    id,
+    payload,
+  }: InterviewUpdateRequestArgumentsDto): Promise<InterviewsGetAllItemResponseDto> {
+    return this.#http.load(
+      `${this.#apiPrefix}${ApiPath.INTERVIEWS}${InterviewsApiPath.ROOT}${id}`,
+      {
+        method: HttpMethod.PUT,
+        contentType: ContentType.JSON,
+        payload: JSON.stringify(payload),
       },
     );
   }
