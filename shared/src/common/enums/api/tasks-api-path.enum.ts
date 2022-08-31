b77a68a6f325406fe4b69138868d6967e289d8eb
fshabanov <fshabanov01@gmail.com>
2022-08-31 12:01:31 +0400

    GRHB-346: * api path typo

--- shared/src/common/enums/api/tasks-api-path.enum.ts
+++ shared/src/common/enums/api/tasks-api-path.enum.ts
@@ -1,6 +1,6 @@
 enum TasksApiPath {
   TASKS_$ID = '/:taskId',
-  MODULES_$ID_MENTEES_$ID = '/modules/:moduleId/mentees/menteeId',
+  MODULES_$ID_MENTEES_$ID = '/modules/:moduleId/mentees/:menteeId',
   TASKS_$ID_NOTES = '/:taskId/notes',
 }
 
