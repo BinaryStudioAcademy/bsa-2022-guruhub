ea291daad7cbe5b55358b294ab53b75e8b322d45
Alexandr <1982alexvm@gmail.com>
2022-09-01 17:08:33 +0300

    GRHB-360: * fix - Prefixes for module should be same

--- mobile/src/store/courses/common.ts
+++ mobile/src/store/courses/common.ts
@@ -1,10 +1,10 @@
 enum ActionType {
   ADD_COURSE = 'courses/add-course',
-  BECOME_MENTOR = 'course/become-mentor',
-  CREATE_MENTOR = 'course/create-mentor',
+  BECOME_MENTOR = 'courses/become-mentor',
+  CREATE_MENTOR = 'courses/create-mentor',
   GET_COURSES = 'courses/get-courses',
   GET_COURSE = 'courses/get-course',
-  GET_MENTORS = 'course/get-mentors',
+  GET_MENTORS = 'courses/get-mentors',
   SET_BECOME_MENTOR_INVISIBLE = 'courses/set-become-mentor-invisible',
   UPDATE_VISIBILITY_BECOME_MENTOR = 'courses/update-visibility-become-mentor',
   UPDATE_CATEGORY = 'courses/update-category',
