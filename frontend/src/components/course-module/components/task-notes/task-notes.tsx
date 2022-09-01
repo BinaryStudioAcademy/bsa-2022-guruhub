a42e14071591d2279368dfbd8876db2e8940c6d4
fshabanov <fshabanov01@gmail.com>
2022-09-01 13:44:33 +0400

    GRHB-346: + no notes placeholder

--- frontend/src/components/course-module/components/task-notes/task-notes.tsx
+++ frontend/src/components/course-module/components/task-notes/task-notes.tsx
@@ -1,12 +1,17 @@
 import { FC, TaskNoteGetItemResponseDto } from 'common/types/types';
 
 import { TaskNoteCard } from './components/components';
+import styles from './styles.module.scss';
 
 type Props = {
   notes: TaskNoteGetItemResponseDto[];
 };
 
 const TaskNotes: FC<Props> = ({ notes }) => {
+  if (!notes.length) {
+    return <p className={styles.placeholder}>There are no notes yet.</p>;
+  }
+
   return (
     <div>
       <h1>History</h1>
