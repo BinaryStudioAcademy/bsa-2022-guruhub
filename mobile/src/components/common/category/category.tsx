6a53d5c9f8d90f62e98678a0730e728f2c93f8ac
Revenets <revenets@gmail.com>
2022-09-02 11:40:44 +0300

    GRHB-356: * separate courseCategory and common category

--- mobile/src/components/common/category/category.tsx
+++ mobile/src/components/common/category/category.tsx
@@ -8,42 +8,16 @@ import { styles } from './styles';
 type Props = {
   keyName: string;
   name: string;
-  color?: string;
-  isActive?: boolean;
 };
 
-const Category: FC<Props> = ({ keyName, name, color, isActive }) => {
+const Category: FC<Props> = ({ keyName, name }) => {
   const imageKeys = Object.keys(categoryKeyToImage);
   const hasImage = imageKeys.includes(keyName);
 
   return (
-    <View
-      style={
-        color
-          ? {
-              ...styles.categoryListContainer,
-              ...(isActive && styles.activeItem),
-              borderColor: color,
-            }
-          : styles.container
-      }
-    >
-      {hasImage ? (
-        <CategoryImage name={keyName} />
-      ) : (
-        <View
-          style={{
-            ...styles.logoTextWrapper,
-            backgroundColor: color,
-            borderColor: color,
-          }}
-        >
-          <Text style={styles.logoText}>{name[0].toUpperCase()}</Text>
-        </View>
-      )}
-      <Text style={{ ...styles.text, ...(isActive && styles.activeText) }}>
-        {name}
-      </Text>
+    <View style={styles.container}>
+      {hasImage && <CategoryImage name={keyName} />}
+      <Text style={styles.text}>{name}</Text>
     </View>
   );
 };
--- mobile/src/components/course/components/course-category/course-category.tsx
+++ mobile/src/components/course/components/course-category/course-category.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react';
 
-import { Category, Pressable } from '~/components/common/common';
+import { categoryKeyToImage } from '~/common/maps/maps';
+import { Pressable, Text, View } from '~/components/common/common';
 import { useEffect, useState } from '~/hooks/hooks';
 
+import { CategoryImage } from './components/components';
 import { getRandomColor } from './helpers/get-random-color.helper';
+import { styles } from './style';
 
 type Props = {
   keyName: string;
@@ -14,6 +17,8 @@ type Props = {
 
 const CourseCategory: FC<Props> = ({ isActive, keyName, name, onPress }) => {
   const [color, setColor] = useState('');
+  const imageKeys = Object.keys(categoryKeyToImage);
+  const hasImage = imageKeys.includes(keyName);
 
   const handlePress = (): void => {
     onPress?.();
@@ -25,12 +30,34 @@ const CourseCategory: FC<Props> = ({ isActive, keyName, name, onPress }) => {
 
   return (
     <Pressable onPress={handlePress}>
-      <Category
-        keyName={keyName}
-        name={name}
-        color={color}
-        isActive={isActive}
-      />
+      <View
+        style={
+          color
+            ? {
+                ...styles.categoryListContainer,
+                ...(isActive && styles.activeItem),
+                borderColor: color,
+              }
+            : styles.container
+        }
+      >
+        {hasImage ? (
+          <CategoryImage name={keyName} />
+        ) : (
+          <View
+            style={{
+              ...styles.logoTextWrapper,
+              backgroundColor: color,
+              borderColor: color,
+            }}
+          >
+            <Text style={styles.logoText}>{name[0].toUpperCase()}</Text>
+          </View>
+        )}
+        <Text style={{ ...styles.text, ...(isActive && styles.activeText) }}>
+          {name}
+        </Text>
+      </View>
     </Pressable>
   );
 };
