08d5df701a8e3f143417e9b5058225a41bafbdeb
Yaroslav Lebedenko <nailheart4@gmail.com>
2022-09-03 20:55:32 +0300

    GRHB-357: * remove useless styles

--- mobile/src/components/setting/styles.ts
+++ mobile/src/components/setting/styles.ts
@@ -29,13 +29,6 @@ const styles = StyleSheet.create({
     height: 136,
     marginRight: 20,
   },
-  label: {
-    color: AppColor.TEXT.GRAY_100,
-    fontFamily: AppFontFamily.INTER_500,
-    fontSize: 14,
-    lineHeight: 16,
-    paddingBottom: 9,
-  },
   buttons: {
     alignSelf: 'flex-end',
     marginVertical: 20,
