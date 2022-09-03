6127813437e712134beee70fdc2cdda4faaa6f59
Revenets <revenets@gmail.com>
2022-09-03 20:06:36 +0300

    GRHB-394: * change checkbox style

--- mobile/src/components/common/checkbox/styles.ts
+++ mobile/src/components/common/checkbox/styles.ts
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    // marginTop: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
