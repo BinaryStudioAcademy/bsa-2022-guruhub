53c261ff7e3b70487e1e81885b0e5ff3af0c3937
R-Mielamud <2m.roman2@gmail.com>
2022-09-04 11:32:03 +0300

    GRHB-224: * fix test file name, titles and assertions, added more 403 checks

--- backend/tests/src/tests/specs/users/delete/delete.spec.ts
+++ backend/tests/src/tests/specs/users/delete/delete.spec.ts
@@ -12,6 +12,7 @@ import {
 
 import { JWT_TOKEN_REGEX } from '~/lib/common/constants/constants';
 import { Response } from '~/lib/common/types/types';
+import { withTestData } from '~/lib/helpers/helpers';
 import {
   apiSessionStorage,
   authService,
@@ -27,19 +28,33 @@ import { signUpRequestMock } from '~/tests/mocks/mocks';
 
 describe('Users delete tests', () => {
   const userToDeleteData: UserSignUpRequestDto = signUpRequestMock();
+
+  const noPermissionUsers = {
+    student: testsConfig.users.student,
+    jsMentor: testsConfig.users.jsMentor,
+    pythonMentor: testsConfig.users.pythonMentor,
+    csharpMentor: testsConfig.users.csharpMentor,
+    interviewer: testsConfig.users.interviewer,
+    interviewsManager: testsConfig.users.interviewsManager,
+    categoriesManager: testsConfig.users.categoriesManager,
+    mentoringManager: testsConfig.users.mentoringManager,
+  } as const;
+
   let expectedUserDetails: Pick<UserDetailsResponseDto, 'fullName'>;
   let expectedSignUpResponse: Pick<UserWithPermissions, 'email'>;
   let userToDeleteId = 0;
 
   after(() => {
-    apiSessionStorage.removeSession('user_to_delete');
-    apiSessionStorage.removeSession('student');
-    apiSessionStorage.removeSession('uam_manager');
+    apiSessionStorage.removeSession('userToDelete');
     apiSessionStorage.removeSession('unauthorized');
+
+    Object.keys(noPermissionUsers).forEach((name) =>
+      apiSessionStorage.removeSession(name),
+    );
   });
 
   before('should create test user to be delete', async () => {
-    apiSessionStorage.addAndEnterSession('user_to_delete');
+    apiSessionStorage.addAndEnterSession('userToDelete');
 
     expectedSignUpResponse = {
       email: userToDeleteData.email,
@@ -66,8 +81,32 @@ describe('Users delete tests', () => {
     JWT_TOKEN_REGEX.test(response.body.token).should.be.true;
   });
 
-  before('should log in as a UAM manager', async () => {
-    apiSessionStorage.addAndEnterSession('uam_manager');
+  withTestData(
+    Object.entries(noPermissionUsers).map(([name, loginData]) => ({
+      name,
+      loginData,
+    })),
+    ({ name, loginData }) => {
+      before(`should sign in as ${name}`, async () => {
+        apiSessionStorage.addAndEnterSession(name);
+
+        const response = (await authService.signIn(
+          loginData,
+        )) as Response<UserSignInResponseDto>;
+
+        httpService.setToken(response.body.token);
+
+        apiSessionStorage.exitSession();
+
+        response.should.have.status(HttpCode.OK);
+        response.should.have.normalExecutionTime;
+        response.body.should.have.jsonSchema(signInResponseSchema);
+      });
+    },
+  );
+
+  before('should sign in as a UAM manager', async () => {
+    apiSessionStorage.addAndEnterSession('uamManager');
 
     const response = (await authService.signIn(
       testsConfig.users.uamManager,
@@ -80,81 +119,6 @@ describe('Users delete tests', () => {
     response.should.have.status(HttpCode.OK);
     response.should.have.normalExecutionTime;
     response.body.should.have.jsonSchema(signInResponseSchema);
-  });
-
-  before('should log in as a student', async () => {
-    apiSessionStorage.addAndEnterSession('student');
-
-    const response = (await authService.signIn(
-      testsConfig.users.student,
-    )) as Response<UserSignInResponseDto>;
-
-    httpService.setToken(response.body.token);
-
-    apiSessionStorage.exitSession();
-
-    response.should.have.status(HttpCode.OK);
-    response.should.have.normalExecutionTime;
-    response.body.should.have.jsonSchema(signInResponseSchema);
-  });
-
-  it(`should return an error after deleting user by user without Manage UAM permission:
-        email:${userToDeleteData.email}
-        password:${userToDeleteData.password}`, async () => {
-    apiSessionStorage.enterSession('student');
-
-    const response = (await usersService.delete(
-      userToDeleteId,
-    )) as Response<HttpErrorDto>;
-
-    apiSessionStorage.exitSession();
-
-    response.should.have.status(HttpCode.FORBIDDEN);
-    response.should.have.normalExecutionTime;
-    response.body.should.have.jsonSchema(errorResponseSchema);
-    response.body.error.should.be.equal(HttpStatusMessage.FORBIDDEN);
-    response.body.message.should.be.equal(ExceptionMessage.PERMISSION_LACK);
-  });
-
-  it('should delete user by user with Manage UAM permission', async () => {
-    apiSessionStorage.enterSession('uam_manager');
-
-    const response = await usersService.delete(userToDeleteId);
-
-    apiSessionStorage.exitSession();
-
-    response.should.have.status(HttpCode.OK);
-    response.should.have.normalExecutionTime;
-    response.body.should.be.equal(true);
-  });
-
-  it(`should return ${HttpCode.NOT_FOUND} ${HttpStatusMessage.NOT_FOUND} after second attempt to delete user by user with Manage UAM permission`, async () => {
-    apiSessionStorage.enterSession('uam_manager');
-
-    const response = await usersService.delete(userToDeleteId);
-
-    apiSessionStorage.exitSession();
-
-    response.should.have.status(HttpCode.NOT_FOUND);
-    response.should.have.normalExecutionTime;
-    response.body.should.be.equal(false);
-  });
-
-  it('should not let deleted user to authorize', async () => {
-    apiSessionStorage.enterSession('user_to_delete');
-
-    const response = (await authService.signIn({
-      email: userToDeleteData.email,
-      password: userToDeleteData.password,
-    })) as Response<HttpErrorDto>;
-
-    apiSessionStorage.exitSession();
-
-    response.should.have.status(HttpCode.BAD_REQUEST);
-    response.body.should.have.jsonSchema(errorResponseSchema);
-    response.body.error.should.be.equal(HttpStatusMessage.BAD_REQUEST);
-    response.body.message.should.be.equal(ExceptionMessage.BAD_CREDENTIALS);
-    response.should.have.normalExecutionTime;
   });
 
   it('should reject to delete user for unauthorized user', async () => {
@@ -170,5 +134,68 @@ describe('Users delete tests', () => {
     response.should.have.normalExecutionTime;
     response.body.should.have.jsonSchema(errorResponseSchema);
     response.body.error.should.be.equal(HttpStatusMessage.UNAUTHORIZED);
+  });
+
+  withTestData(
+    Object.keys(noPermissionUsers).map((name) => ({
+      name,
+    })),
+    ({ name }) => {
+      it(`should return ${HttpCode.FORBIDDEN} ${HttpStatusMessage.FORBIDDEN} after deleting user as ${name}`, async () => {
+        apiSessionStorage.enterSession(name);
+
+        const response = (await usersService.delete(
+          userToDeleteId,
+        )) as Response<HttpErrorDto>;
+
+        apiSessionStorage.exitSession();
+
+        response.should.have.status(HttpCode.FORBIDDEN);
+        response.should.have.normalExecutionTime;
+        response.body.should.have.jsonSchema(errorResponseSchema);
+        response.body.error.should.be.equal(HttpStatusMessage.FORBIDDEN);
+        response.body.message.should.be.equal(ExceptionMessage.PERMISSION_LACK);
+      });
+    },
+  );
+
+  it('should delete user as user with Manage UAM permission', async () => {
+    apiSessionStorage.enterSession('uamManager');
+
+    const response = await usersService.delete(userToDeleteId);
+
+    apiSessionStorage.exitSession();
+
+    response.should.have.status(HttpCode.OK);
+    response.should.have.normalExecutionTime;
+    response.body.should.be.equal(true);
+  });
+
+  it(`should return ${HttpCode.NOT_FOUND} ${HttpStatusMessage.NOT_FOUND} after second attempt to delete user`, async () => {
+    apiSessionStorage.enterSession('uamManager');
+
+    const response = await usersService.delete(userToDeleteId);
+
+    apiSessionStorage.exitSession();
+
+    response.should.have.status(HttpCode.NOT_FOUND);
+    response.should.have.normalExecutionTime;
+  });
+
+  it('should not let deleted user to sign in', async () => {
+    apiSessionStorage.enterSession('userToDelete');
+
+    const response = (await authService.signIn({
+      email: userToDeleteData.email,
+      password: userToDeleteData.password,
+    })) as Response<HttpErrorDto>;
+
+    apiSessionStorage.exitSession();
+
+    response.should.have.status(HttpCode.BAD_REQUEST);
+    response.body.should.have.jsonSchema(errorResponseSchema);
+    response.body.error.should.be.equal(HttpStatusMessage.BAD_REQUEST);
+    response.body.message.should.be.equal(ExceptionMessage.BAD_CREDENTIALS);
+    response.should.have.normalExecutionTime;
   });
 });
