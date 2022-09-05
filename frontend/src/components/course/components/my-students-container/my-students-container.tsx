5e5f5b65a888ad46304ecd719d9863f082e9c7c0
SomMariya <m.som-2030@kpi.ua>
2022-09-05 22:38:12 +0300

    GRHB-396: * fixed issues

--- frontend/src/components/course/components/my-students-container/my-students-container.tsx
+++ frontend/src/components/course/components/my-students-container/my-students-container.tsx
@@ -9,32 +9,29 @@ type Props = {
 };
 
 const MyStudentsContainer: FC<Props> = ({ mentees }) => {
-  if (!mentees.length) {
-    return (
-      <>
-        <h2 className={styles.studentsTitle}>My Students</h2>
-        <p className={styles.placeholder}>There are no students yet...</p>
-      </>
-    );
-  }
+  const hasMentees = Boolean(mentees.length);
 
   return (
     <div className={styles.studentsWrapper}>
       <h2 className={styles.studentsTitle}>My Students</h2>
-      {mentees.map(({ id, fullName, avatar }) => (
-        <div key={id} className={styles.student}>
-          <div className={styles.imageWrapper}>
-            <Image
-              width="74"
-              height="74"
-              src={avatar?.url ?? defaultUserAvatar}
-              alt="user avatar"
-              isCircular
-            />
+      {hasMentees ? (
+        mentees.map(({ id, fullName, avatar }) => (
+          <div key={id} className={styles.student}>
+            <div className={styles.imageWrapper}>
+              <Image
+                width="74"
+                height="74"
+                src={avatar?.url ?? defaultUserAvatar}
+                alt="user avatar"
+                isCircular
+              />
+            </div>
+            <p className={styles.text}>{fullName}</p>
           </div>
-          <p className={styles.text}>{fullName}</p>
-        </div>
-      ))}
+        ))
+      ) : (
+        <p className={styles.placeholder}>There are no students yet...</p>
+      )}
     </div>
   );
 };
