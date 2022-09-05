f9347bf4c39b7d5395196af2f916d0fe869929fa
SomMariya <m.som-2030@kpi.ua>
2022-09-05 11:49:48 +0300

    GRHB-352: * fixed file name

--- backend/src/common/enums/enums.ts
+++ backend/src/common/enums/enums.ts
@@ -15,6 +15,7 @@ export {
   UsersApiPath,
 } from './api/api';
 export { AppEnvironment, ENV, LogLevel } from './app/app';
+export { ChatMessageStatus } from './chat/chat';
 export { CourseHost } from './course/course';
 export { DbTableName } from './db/db';
 export { CustomExceptionName, ExceptionMessage } from './exceptions/exceptions';
@@ -28,4 +29,3 @@ export { StringCase } from './string/string';
 export { TaskStatus } from './task/task';
 export { UserGender } from './user/user';
 export { VendorKey } from './vendor/vendor';
-export { ChatMessageStatus } from './сhat/chat';
