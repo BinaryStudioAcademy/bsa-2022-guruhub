1c17cf6b27b9c6d08f4af21eeeb85258c33ca9e7
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-09-05 18:47:38 +0300

    GRHB-410: * Task does not exist is an info notification now

--- frontend/src/store/course-module/actions.ts
+++ frontend/src/store/course-module/actions.ts
@@ -11,6 +11,7 @@ import {
   TaskNoteGetItemResponseDto,
   TaskNoteManipulateRequestDto,
 } from 'common/types/types';
+import { notification } from 'services/services';
 
 import { ActionType } from './common';
 
@@ -44,12 +45,18 @@ const createNote = createAsyncThunk<
 });
 
 const getTask = createAsyncThunk<
-  TaskGetItemReponseDto,
+  TaskGetItemReponseDto | null,
   TaskGetByMenteeIdAndModuleId,
   AsyncThunkConfig
 >(ActionType.GET_TASK, async ({ menteeId, moduleId }, { extra }) => {
   const { tasksApi } = extra;
   const task = await tasksApi.getByMenteeIdAndModuleId({ menteeId, moduleId });
+
+  if (!task) {
+    notification.info(NotificationMessage.TASK_DOES_NOT_EXIST);
+
+    return null;
+  }
 
   return task;
 });
