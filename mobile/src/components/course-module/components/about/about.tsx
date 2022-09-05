830d2dd7e967c6980f580a681b534da1d3c71393
alexandr <1982alexvm@gmail.com>
2022-09-06 01:37:19 +0300

    GRHB-398: + about screen

--- mobile/src/components/course-module/components/about/about.tsx
+++ mobile/src/components/course-module/components/about/about.tsx
@@ -1,25 +1,44 @@
 import React, { FC } from 'react';
 
 import { DataStatus } from '~/common/enums/enums';
-import { Spinner, Text, View } from '~/components/common/common';
-import { useAppSelector } from '~/hooks/hooks';
+import {
+  Content,
+  ScrollView,
+  Spinner,
+  Text,
+  View,
+} from '~/components/common/common';
+import { useAppSelector, useWindowDimensions } from '~/hooks/hooks';
 
 import { styles } from './styles';
 
 const About: FC = () => {
-  const { courseTitle, dataStatus } = useAppSelector(({ courseModules }) => ({
-    courseTitle: courseModules.module?.courseTitle ?? '',
-    dataStatus: courseModules.dataModuleStatus,
-  }));
+  const { courseTitle, moduleTitle, moduleDescription, dataStatus } =
+    useAppSelector(({ courseModules }) => ({
+      courseTitle: courseModules.module?.courseTitle ?? 'Untitled',
+      moduleTitle: courseModules.module?.title ?? 'Untitled',
+      moduleDescription: courseModules.module?.description ?? 'No description',
+      dataStatus: courseModules.dataModuleStatus,
+    }));
+
+  const { width } = useWindowDimensions();
 
   if (dataStatus === DataStatus.PENDING) {
     return <Spinner isOverflow />;
   }
 
   return (
-    <View style={styles.wrapper}>
+    <ScrollView style={styles.wrapper}>
       <Text style={styles.courseTitle}>{courseTitle}</Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.moduleTitle}>{moduleTitle}</Text>
+        <Content
+          html={moduleDescription}
+          width={width}
+          style={styles.description}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
--- mobile/src/components/course-module/components/about/styles.ts
+++ mobile/src/components/course-module/components/about/styles.ts
@@ -7,11 +7,30 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  container: {
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: AppColor.BACKGROUND.GRAY_300,
+  },
   courseTitle: {
     fontFamily: AppFontFamily.INTER_600,
     color: AppColor.TEXT.GRAY_100,
     fontSize: 24,
     lineHeight: 34,
+    paddingBottom: 20,
+  },
+  moduleTitle: {
+    fontFamily: AppFontFamily.INTER_600,
+    color: AppColor.TEXT.GRAY_100,
+    fontSize: 16,
+    lineHeight: 24,
+    marginBottom: 7,
+  },
+  description: {
+    fontFamily: AppFontFamily.INTER_400,
+    color: AppColor.TEXT.GRAY_200,
+    fontSize: 14,
+    lineHeight: 24,
   },
 });
 
