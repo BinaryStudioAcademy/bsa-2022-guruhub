f4e6fb6f0d60bb84678f445a0db133245602a264
Alina Kupchyk <kalinkaaaa14@gmail.com>
2022-09-05 14:33:03 +0300

    GRHB384: + title renamed

--- frontend/src/components/course-categories/course-categories.tsx
+++ frontend/src/components/course-categories/course-categories.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.scss';
 const CourseCategories: FC = () => {
   return (
     <div className={styles.courseCategories}>
-      <h1 className={styles.pageTitle}>Course with categories</h1>
+      <h1 className={styles.pageTitle}>Course settings</h1>
       <CourseCategoriesTable />
     </div>
   );
