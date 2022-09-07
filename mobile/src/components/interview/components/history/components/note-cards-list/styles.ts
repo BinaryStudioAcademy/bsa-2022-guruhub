7e645a27a778bb7830625cdaca7ab4bb4d559bf4
Revenets <revenets@gmail.com>
2022-09-07 16:37:06 +0300

    GRHB-463: * add history no notes placeholder

--- mobile/src/components/interview/components/history/components/note-cards-list/styles.ts
+++ mobile/src/components/interview/components/history/components/note-cards-list/styles.ts
@@ -1,8 +1,18 @@
 import { StyleSheet } from 'react-native';
 
+import { AppColor, AppFontFamily } from '~/common/enums/enums';
+
 const styles = StyleSheet.create({
   container: {
     marginVertical: 25,
+  },
+  noNotes: {
+    fontSize: 24,
+    fontFamily: AppFontFamily.INTER_400_ITALIC,
+    color: AppColor.TEXT.GRAY_200,
+    textAlign: 'center',
+    paddingHorizontal: 20,
+    marginTop: 150,
   },
 });
 
