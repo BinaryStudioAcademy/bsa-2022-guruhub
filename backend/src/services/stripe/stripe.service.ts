c730c6c1bcb42caf0da8e5b7398af6940cc78b7a
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-09-07 18:48:12 +0300

    GRHB-442: + stripe lib, envs, basic service config

---
+++ backend/src/services/stripe/stripe.service.ts
@@ -0,0 +1,15 @@
+import StripeApi from 'stripe';
+
+type Constructor = {
+  secretKey: string;
+};
+
+class Stripe {
+  #stripe: StripeApi;
+
+  public constructor({ secretKey }: Constructor) {
+    this.#stripe = new StripeApi(secretKey, { apiVersion: '2022-08-01' });
+  }
+}
+
+export { Stripe };
