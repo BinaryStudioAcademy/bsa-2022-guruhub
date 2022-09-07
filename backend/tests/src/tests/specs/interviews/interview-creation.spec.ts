78eba45d4c7e68a93b289243afc004fa402a9d23
Yevhen Zavelia <97022343+Eugene-Barcode@users.noreply.github.com>
2022-09-07 15:00:39 +0300

    Update backend/tests/src/tests/specs/interviews/interview-creation.spec.ts
    Co-authored-by: Roman Mielamud <2m.roman2@gmail.com>

--- backend/tests/src/tests/specs/interviews/interview-creation.spec.ts
+++ backend/tests/src/tests/specs/interviews/interview-creation.spec.ts
@@ -146,9 +146,11 @@ describe('Interview creating flow', () => {
     response.should.have.status(HttpCode.OK);
     response.should.have.normalExecutionTime;
     response.body.should.have.jsonSchema(signInResponseSchema);
+
     response.body.user.email.should.be.equal(
       testsConfig.users.interviewsManager.email,
     );
+
     httpService.setToken(response.body.token);
   });
 
