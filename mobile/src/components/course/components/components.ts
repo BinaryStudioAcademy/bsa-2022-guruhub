0294fb947ca958e8e0650f6b7c026d5761435a35
alexandr <1982alexvm@gmail.com>
2022-09-12 21:24:17 +0300

    GRHB-435: * rename edit-category-dropdown dir

--- mobile/src/components/course/components/components.ts
+++ mobile/src/components/course/components/components.ts
@@ -1,6 +1,6 @@
 export { CourseCategory } from './course-category/course-category';
 export { CourseDescription } from './course-description/course-description';
 export { CourseStudents } from './course-students/course-students';
-export { EditCategory } from './edit-category-dropdown/edit-category';
+export { EditCategory } from './edit-category/edit-category';
 export { EditCourseCategory } from './edit-course-category/edit-course-category';
 export { MyMentor } from './my-mentor/my-mentor';
