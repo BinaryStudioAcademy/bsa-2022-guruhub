f927c69d72f8e94efcf5a6e73a200da107a5931b
Revenets <revenets@gmail.com>
2022-09-15 20:21:32 +0300

    GRHB-506: * set edge to drawer on my courses screen

--- mobile/src/navigation/app/common/constants/constants.ts
+++ mobile/src/navigation/app/common/constants/constants.ts
@@ -69,6 +69,9 @@ const NAVIGATION_ITEMS: DrawerNavigationItem[] = [
     permissions: [],
     isAuthRequired: true,
     drawerGroup: 'Menu',
+    screenOptions: {
+      swipeEdgeWidth: 10,
+    },
   },
   {
     name: AppScreenName.INTERVIEWS,
