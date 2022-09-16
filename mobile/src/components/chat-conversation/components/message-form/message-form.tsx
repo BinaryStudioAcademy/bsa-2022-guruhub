a521a7ca3e6f629fd1d843937782c0d23ea8e594
Revenets <revenets@gmail.com>
2022-09-16 11:26:45 +0300

    GRHB-598: * change chat input rows logic

--- mobile/src/components/chat-conversation/components/message-form/message-form.tsx
+++ mobile/src/components/chat-conversation/components/message-form/message-form.tsx
@@ -3,7 +3,13 @@ import React, { FC } from 'react';
 import { AppColor } from '~/common/enums/enums';
 import { ChatMessageFormRequestDto } from '~/common/types/types';
 import { Icon, Input, Pressable, View } from '~/components/common/common';
-import { useAppForm, useCallback, useFocusEffect } from '~/hooks/hooks';
+import {
+  useAppForm,
+  useCallback,
+  useEffect,
+  useFocusEffect,
+  useState,
+} from '~/hooks/hooks';
 import { chatMessageCreate } from '~/validation-schemas/validation-schemas';
 
 import { getDefaultMessagePayload } from './helpers/helpers';
@@ -14,18 +20,30 @@ type Props = {
 };
 
 const MessageForm: FC<Props> = ({ onSubmit }) => {
-  const { control, errors, handleSubmit, reset } =
+  const { control, errors, handleSubmit, reset, watch } =
     useAppForm<ChatMessageFormRequestDto>({
       defaultValues: getDefaultMessagePayload(),
       validationSchema: chatMessageCreate,
     });
+
+  const [messageChar, setMessageChar] = useState<string>();
+  const [rowsCount, setRowsCount] = useState<number>(1);
 
   const hitSlop = { top: 5, bottom: 5, left: 5, right: 5 };
 
   const handleSend = (payload: ChatMessageFormRequestDto): void => {
     onSubmit(payload);
     reset({ message: '' });
+    setRowsCount(1);
   };
+
+  useEffect(() => {
+    setMessageChar(watch.name);
+
+    if (messageChar === '\n') {
+      setRowsCount(rowsCount + 1);
+    }
+  }, [messageChar]);
 
   useFocusEffect(
     useCallback(() => {
@@ -43,7 +61,7 @@ const MessageForm: FC<Props> = ({ onSubmit }) => {
           name="message"
           control={control}
           errors={errors}
-          rows={3}
+          rows={rowsCount}
         />
       </View>
       <Pressable
