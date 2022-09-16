e48c84f09ffad14252168247532acdbb38acf902
Kirill Katkov <katkov.kirill10@gmail.com>
2022-09-16 14:27:26 +0300

    GRHB-579: * minor naming upd

--- shared/src/helpers/string/remove-hashtag-from-id/remove-hashtag-from-id.helper.ts
+++ shared/src/helpers/string/remove-hashtag-from-id/remove-hashtag-from-id.helper.ts
@@ -1,7 +1,7 @@
 const removeHashtagFromId = (stringToChange: string): number => {
-  const amount = 1;
+  const hashtagIndex = 1;
 
-  return Number(stringToChange.slice(amount));
+  return Number(stringToChange.slice(hashtagIndex));
 };
 
 export { removeHashtagFromId };
