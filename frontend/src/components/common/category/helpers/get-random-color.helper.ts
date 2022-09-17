cdc23812d8d968bd0bb4d087a6112c2132f195d7
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-09-17 15:09:40 +0300

    GRHB-614: * added one more color

--- frontend/src/components/common/category/helpers/get-random-color.helper.ts
+++ frontend/src/components/common/category/helpers/get-random-color.helper.ts
@@ -1,5 +1,5 @@
 const getRandomColor = (): string => {
-  const colors = ['#F6C648', '#2563EB', '#15BE4F', '#C565C7'];
+  const colors = ['#F6C648', '#2563EB', '#15BE4F', '#C565C7', '#FEEAAA'];
 
   return colors[Math.floor(Math.random() * colors.length)];
 };
