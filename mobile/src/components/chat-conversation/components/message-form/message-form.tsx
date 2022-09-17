7e19b73ba64ddd33ce272d19533598cc8961e554
Oleksandr Borysenko <3pc@ukr.net>
2022-09-17 12:38:50 +0300

    GRHB-524: * resolve: Send button is available after opening the chat

--- mobile/src/components/chat-conversation/components/message-form/message-form.tsx
+++ mobile/src/components/chat-conversation/components/message-form/message-form.tsx
@@ -2,7 +2,13 @@ import React, { FC } from 'react';
 
 import { AppColor } from '~/common/enums/enums';
 import { ChatMessageFormRequestDto } from '~/common/types/types';
-import { Icon, Input, Pressable, View } from '~/components/common/common';
+import {
+  Icon,
+  Input,
+  Pressable,
+  ScrollView,
+  View,
+} from '~/components/common/common';
 import {
   useAppForm,
   useCallback,
@@ -29,9 +35,10 @@ const MessageForm: FC<Props> = ({ onSubmit }) => {
 
   const [messageChar, setMessageChar] = useState<string>();
   const [rowsCount, setRowsCount] = useState<number>(1);
+  const [disabledButtom, setDisabledButtom] = useState<boolean>(true);
 
   const hitSlop = { top: 5, bottom: 5, left: 5, right: 5 };
-  const hasError = Boolean(errors.message?.message);
+  const hasError = Boolean(errors.message?.message) || disabledButtom;
 
   const handleSend = (payload: ChatMessageFormRequestDto): void => {
     onSubmit(payload);
@@ -47,6 +54,14 @@ const MessageForm: FC<Props> = ({ onSubmit }) => {
     }
   }, [messageChar]);
 
+  useEffect(() => {
+    watch(({ message }: { message: string }) => {
+      if (message) {
+        setDisabledButtom(false);
+      } else setDisabledButtom(true);
+    });
+  }, [watch]);
+
   useFocusEffect(
     useCallback(() => {
       return () => {
@@ -56,30 +71,32 @@ const MessageForm: FC<Props> = ({ onSubmit }) => {
   );
 
   return (
-    <View style={styles.container}>
-      <View style={styles.input}>
-        <Input
-          placeholder="Type a message"
-          name="message"
-          control={control}
-          errors={errors}
-          rows={rowsCount}
-        />
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.input}>
+          <Input
+            placeholder="Type a message"
+            name="message"
+            control={control}
+            errors={errors}
+            rows={rowsCount}
+          />
+        </View>
+        <Pressable
+          onPress={handleSubmit(handleSend)}
+          hitSlop={hitSlop}
+          style={[styles.button, hasError && styles.disabledButton]}
+          disabled={hasError}
+        >
+          <Icon
+            color={AppColor.BRAND.BLUE_100}
+            width={25}
+            height={25}
+            name="send"
+          />
+        </Pressable>
       </View>
-      <Pressable
-        onPress={handleSubmit(handleSend)}
-        hitSlop={hitSlop}
-        style={[styles.button, hasError && styles.disabledButton]}
-        disabled={hasError}
-      >
-        <Icon
-          color={AppColor.BRAND.BLUE_100}
-          width={25}
-          height={25}
-          name="send"
-        />
-      </Pressable>
-    </View>
+    </ScrollView>
   );
 };
 
