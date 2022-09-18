f642fe30390ba25e607e9697f1e2c921672e74ba
fshabanov <fshabanov01@gmail.com>
2022-09-18 15:13:01 +0400

    GRHB-619: * update interview status to enum migration

--- backend/src/data/migrations/20220918074339_change_interview_status_to_enum.ts
+++ backend/src/data/migrations/20220918074339_change_interview_status_to_enum.ts
@@ -1,6 +1,15 @@
 import { Knex } from 'knex';
 
 enum InterviewStatus {
+  PENDING = 'pending',
+  IN_PROGRESS = 'in-progress',
+  COMPLETED = 'completed',
+  REJECTED = 'rejected',
+  NEW = 'new',
+  CANCELED = 'canceled',
+}
+
+enum InterviewStatusUppercase {
   PENDING = 'Pending',
   IN_PROGRESS = 'In Progress',
   COMPLETED = 'Completed',
@@ -17,7 +26,17 @@ enum ColumnName {
 }
 
 async function up(knex: Knex): Promise<void> {
-  return knex.schema.raw(`
+  await knex.schema.raw(`
+    UPDATE interviews SET status = (CASE WHEN status = '${InterviewStatusUppercase.PENDING}' THEN '${InterviewStatus.PENDING}'
+      WHEN status = '${InterviewStatusUppercase.IN_PROGRESS}' THEN '${InterviewStatus.IN_PROGRESS}'
+      WHEN status = '${InterviewStatusUppercase.COMPLETED}' THEN '${InterviewStatus.COMPLETED}'
+      WHEN status = '${InterviewStatusUppercase.REJECTED}' THEN '${InterviewStatus.REJECTED}'
+      WHEN status = '${InterviewStatusUppercase.NEW}' THEN '${InterviewStatus.NEW}'
+      WHEN status = '${InterviewStatusUppercase.CANCELED}' THEN '${InterviewStatus.CANCELED}'
+      ELSE status
+  END);`);
+
+  await knex.schema.raw(`
     ALTER TABLE "${TABLE_NAME}"
     ADD CONSTRAINT "${ColumnName.STATUS_CHECK_CONSTRAINT}"
     CHECK (${ColumnName.STATUS} IN ('${InterviewStatus.PENDING}', '${InterviewStatus.IN_PROGRESS}', '${InterviewStatus.COMPLETED}', '${InterviewStatus.REJECTED}', '${InterviewStatus.NEW}', '${InterviewStatus.CANCELED}'))
@@ -25,10 +44,20 @@ async function up(knex: Knex): Promise<void> {
 }
 
 async function down(knex: Knex): Promise<void> {
-  return knex.schema.raw(`
+  await knex.schema.raw(`
     ALTER TABLE "${TABLE_NAME}"
     DROP CONSTRAINT "${ColumnName.STATUS_CHECK_CONSTRAINT}"
   `);
+
+  await knex.schema.raw(`
+    UPDATE interviews SET status = (CASE WHEN status = '${InterviewStatus.PENDING}' THEN '${InterviewStatusUppercase.PENDING}'
+      WHEN status = '${InterviewStatus.IN_PROGRESS}' THEN '${InterviewStatusUppercase.IN_PROGRESS}'
+      WHEN status = '${InterviewStatus.COMPLETED}' THEN '${InterviewStatusUppercase.COMPLETED}'
+      WHEN status = '${InterviewStatus.REJECTED}' THEN '${InterviewStatusUppercase.REJECTED}'
+      WHEN status = '${InterviewStatus.NEW}' THEN '${InterviewStatusUppercase.NEW}'
+      WHEN status = '${InterviewStatus.CANCELED}' THEN '${InterviewStatusUppercase.CANCELED}'
+      ELSE status
+  END);`);
 }
 
 export { down, up };
