2a1ee17c58b5b7772c838bb74058d04a153b1e64
SomMariya <m.som-2030@kpi.ua>
2022-09-20 10:33:58 +0300

    GRHB-646: + added perm check to other applications

--- frontend/src/components/interview/components/other-applications-table/components/id-cell/id-cell.tsx
+++ frontend/src/components/interview/components/other-applications-table/components/id-cell/id-cell.tsx
@@ -1,13 +1,25 @@
-import { AppRoute } from 'common/enums/enums';
-import { FC } from 'common/types/types';
+import { AppRoute, PermissionKey } from 'common/enums/enums';
+import { FC, UserWithPermissions } from 'common/types/types';
 import { Link } from 'components/common/common';
 import { OtherApplicationsTableRow } from 'components/interview/common/types/types';
-import { generateDynamicPath } from 'helpers/helpers';
+import { checkHasPermission, generateDynamicPath } from 'helpers/helpers';
+import { useAppSelector } from 'hooks/hooks';
 import { CellProps } from 'react-table';
 
 import styles from './styles.module.scss';
 
 const IdCell: FC<CellProps<OtherApplicationsTableRow>> = ({ value }) => {
+  const { user } = useAppSelector((state) => state.auth);
+
+  const hasPermission = checkHasPermission({
+    permissionKeys: [PermissionKey.MANAGE_INTERVIEWS],
+    userPermissions: (user as UserWithPermissions).permissions,
+  });
+
+  if (!hasPermission) {
+    return <span>#{value}</span>;
+  }
+
   return (
     <Link
       to={generateDynamicPath(AppRoute.INTERVIEWS_$ID, { id: value })}
