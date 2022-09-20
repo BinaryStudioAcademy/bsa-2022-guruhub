ecfe05f5f84413ba2fe8656f0b4773d54786d05a
Kirill Katkov <katkov.kirill10@gmail.com>
2022-09-20 11:40:11 +0300

    GRHB-652: + added required balance to error msg

--- backend/src/services/billing/billing.service.ts
+++ backend/src/services/billing/billing.service.ts
@@ -97,8 +97,15 @@ class Billing {
       rawPriceOfStudying * Billing.DEFAULT_STUDYING_PRICE_COEFFICIENT;
 
     if (menteeBalance < priceOfStudying) {
+      const requiredBalance = ` You need to add $${Math.round(
+        priceOfStudying,
+      )} to your balance.`;
+
       throw new BillingError({
-        message: ExceptionMessage.NOT_ENOUGH_FUNDS_TO_PAY_FOR_MENTORS_SERVICES,
+        message:
+          ExceptionMessage.NOT_ENOUGH_FUNDS_TO_PAY_FOR_MENTORS_SERVICES.concat(
+            requiredBalance,
+          ),
       });
     }
 
