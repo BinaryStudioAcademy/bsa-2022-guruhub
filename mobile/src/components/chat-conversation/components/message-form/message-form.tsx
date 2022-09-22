ec77e519914356946138b26b06348077214211c3
Oleksandr Borysenko <3pc@ukr.net>
2022-09-22 13:09:23 +0300

    GRHB-524: * add trimming spaces for text field

--- mobile/src/components/chat-conversation/components/message-form/message-form.tsx
+++ mobile/src/components/chat-conversation/components/message-form/message-form.tsx
@@ -33,7 +33,9 @@ const MessageForm: FC<Props> = ({ onSubmit }) => {
   const isButtonDisabled = hasError || Boolean(!messageFieldValue.trim());
 
   const handleSend = (payload: ChatMessageFormRequestDto): void => {
-    onSubmit(payload);
+    const { message } = payload;
+
+    onSubmit({ message: message.trim() });
     reset({ message: '' });
     setRowsCount(1);
   };
