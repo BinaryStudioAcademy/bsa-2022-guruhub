6e79fd1ec0612e39fdcdd0937b7f7e5ab73dba18
fshabanov <fshabanov01@gmail.com>
2022-09-22 14:54:14 +0400

    GRHB-618: + breakpoints

--- frontend/src/components/dashboard/common.ts
+++ frontend/src/components/dashboard/common.ts
@@ -1,17 +1,33 @@
 import { CarouselResponsiveType } from 'common/types/types';
 
 const carouselResponsiveBreakpoints: CarouselResponsiveType = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1480 },
+  desktop3XLarge: {
+    breakpoint: { max: 2870, min: 2560 },
+    items: 7,
+  },
+  desktop2XLarge: {
+    breakpoint: { max: 2560, min: 2250 },
+    items: 6,
+  },
+  desktopXLarge: {
+    breakpoint: { max: 2250, min: 1940 },
     items: 5,
   },
-  tablet: {
-    breakpoint: { max: 1480, min: 464 },
+  desktopLarge: {
+    breakpoint: { max: 1940, min: 1630 },
+    items: 4,
+  },
+  desktop: {
+    breakpoint: { max: 1630, min: 1320 },
     items: 3,
   },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
+  tablet: {
+    breakpoint: { max: 1320, min: 1010 },
     items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 1010, min: 0 },
+    items: 1,
   },
 };
 
