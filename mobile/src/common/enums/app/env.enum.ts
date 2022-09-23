d2c397e9f0b93e3246ddf70eebbe42aa6e71234f
Volodymyr Minchenko <voldemarprk2001@gmail.com>
2022-09-23 08:10:43 +0300

    development: * stripe public key for mobile

--- mobile/src/common/enums/app/env.enum.ts
+++ mobile/src/common/enums/app/env.enum.ts
@@ -1,14 +1,13 @@
 import Config from 'react-native-config';
 
-const { API_ORIGIN_URL, APP_HOST, APP_SCHEME, REACT_APP_STRIPE_PUBLIC_KEY } =
-  Config;
+const { API_ORIGIN_URL, APP_HOST, APP_SCHEME, STRIPE_PUBLIC_KEY } = Config;
 
 const ENV = {
   APP: {
     API_PATH: API_ORIGIN_URL ?? '',
     APP_HOST: APP_HOST ?? '',
     APP_SCHEME: APP_SCHEME ?? '',
-    REPLENISH_PUBLIC_KEY: REACT_APP_STRIPE_PUBLIC_KEY ?? '',
+    REPLENISH_PUBLIC_KEY: STRIPE_PUBLIC_KEY ?? '',
   },
 } as const;
 
