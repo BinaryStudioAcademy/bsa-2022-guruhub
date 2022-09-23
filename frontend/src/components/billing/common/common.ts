81b9abc7c75799779e65ff1ed79e7212467223a7
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-09-23 18:14:08 +0300

    GRHB-653: * removed unnecessary replenish value cards and changed cards placement structure to grid

--- frontend/src/components/billing/common/common.ts
+++ frontend/src/components/billing/common/common.ts
@@ -6,13 +6,10 @@ const REPLENISH_PUBLIC_KEY = ENV.REPLENISH_PUBLIC_KEY;
 
 const DEFAULT_REPLENISH_AMOUNTS: readonly ReplenishAmount[] = [
   { id: 1, value: 50 },
-  { id: 2, value: 75 },
-  { id: 3, value: 100 },
-  { id: 4, value: 150 },
-  { id: 5, value: 300 },
-  { id: 6, value: 500 },
-  { id: 7, value: 1000 },
-  { id: 8, value: 3000 },
+  { id: 2, value: 100 },
+  { id: 3, value: 300 },
+  { id: 4, value: 500 },
+  { id: 5, value: 1000 },
 ] as const;
 
 export { DEFAULT_REPLENISH_AMOUNTS, REPLENISH_PUBLIC_KEY };
